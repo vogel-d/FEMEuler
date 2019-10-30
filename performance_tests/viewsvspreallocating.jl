d7440ae4899df6de9e2fc2a3635e7608b7809f81
rug-levin <levinrug98@gmail.com>
2019-10-30 15:28:31 +0100

    correcting the test to benchmark functions, result: seems to have no actual benefits

--- performance_tests/viewsvspreallocating.jl
+++ performance_tests/viewsvspreallocating.jl
@@ -1,43 +1,45 @@
 using BenchmarkTools
 
-vector=rand(1000000+5)
-
-testview = @benchmark begin
+#meantime: 1.903ms
+#allocs estimate: 2
+#memory estimate: 7.63MiB
+function testview()
+    vector=rand(1000000+5)
     sum1=0.0;
+
     for i in 1:1000000
-        a= @views vector[i:i+5];
+        a= @view vector[i:i+5];
         #sum1+=sum(a);
     end
 end
 
-
-testpreallocatearray = @benchmark begin
+#meantime: 9.429ms
+#allocs estimate: 3
+#memory estimate: 7.63MiB
+function testpreallocatearray()
+    vector=rand(1000000+5)
     sum1=0.0
     a=ones(6);
+
     for i in 1:1000000
-        a=vector[i:i+5];
+        for j in 0:5
+            a[j+1]=vector[i+j]
+        end
+        #a=vector[i:i+5];
         #sum1+=sum(a);
     end
 end
 
-testpreallocateview = @benchmark begin
+#meantime: 1.871ms
+#allocs estimate: 2
+#memory estimate: 7.63MiB
+function testpreallocateview()
+    vector=rand(1000000+5)
     sum1=0.0
     a = @views vector[1:6]
+    
     for i in 1:1000000
         a= @views vector[i:i+5];
         #sum1+=sum(a);
     end
 end
-
-println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
-println("Old Version: creating a new view in every loop-iteration\n")
-display(testview);
-println("\n\n");
-
-println("New try #1: preallocating an array, not using @views\n")
-display(testpreallocatearray);
-println("\n\n");
-
-println("New try #2: preallocating a view\n")
-display(testpreallocateview);
-println("\n\n");
