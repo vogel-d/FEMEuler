5981433bc38b54cde0294e86855972870cef1042
vogel-d <denise.vogel@gmx.net>
2019-12-13 14:57:38 +0100

    changed to be executed before the generation of the degFs

--- adaptGeometry.jl
+++ adaptGeometry.jl
@@ -1,132 +1,77 @@
-function adaptGeometry!(p::femProblem,vid::Array{Int64,1},c::Array{Float64,2})
-    p.mesh.geometry.coordinates[:,vid]=c;
-    b=getBoundary(p.mesh);
-
-    for k in keys(p.degF)
-            p.degF[k]=degF(p.mesh, k, b, p.boundary,p.kubPoints);
+function adaptGeometry!(m::mesh,hm::Float64,a::Float64)
+    H=m.geometry.r[2];
+    h(x)=(hm*a^2)/(x^2+a^2); #Witch of Agnesi
+    coord=m.geometry.coordinates;
+    for i in 1:(m.topology.n[1]+1)
+        coord[2,i]=h(coord[1,i]);
     end
-
-    if !isempty(p.massM)
-            assembMass!(p);
+    for i in (m.topology.n[1]+2):size(coord,2)
+        z0=h(coord[1,i])
+        coord[2,i]=H*(coord[2,i]+z0)/(H+z0); #Gal-Chen & Sommerville transformation
     end
-    if !isempty(p.stiffM)
-            assembStiff!(p);
+    return nothing;
+end
+
+function adaptGeometry!(m::mesh,vid::Array{Int64,1},c::Array{Float64,2})
+    m.geometry.coordinates[:,vid]=c;
+    return nothing;
+end
+
+function adaptGeometry!(m::mesh,r::Float64)
+    coord=m.geometry.coordinates;
+
+    for i in 1:size(coord,2)
+        x=r*rand([-1,1])*rand(1)[1];
+        y=rand([-1,1])*sqrt(r*r-x*x)*rand(1)[1];
+        coord[:,i]=[coord[1,i]+x, coord[2,i]+y];
+    end
+    return nothing;
+end
+
+function adaptGeometry!(m::mesh,r::Float64,bv::Set{Int64})
+    coord=m.geometry.coordinates;
+
+    for i in 1:size(coord,2)
+        if !in(i,bv)
+            x=r*rand([-1,1])*rand(1)[1];
+            y=rand([-1,1])*sqrt(r*r-x*x)*rand(1)[1];
+            coord[:,i]=[coord[1,i]+x, coord[2,i]+y];
+        end
     end
 
     return nothing;
 end
 
-function adaptGeometry!(p::femProblem,r::Float64,adaptBoundary::Bool)
-        coord=p.mesh.geometry.coordinates;
-        dim=p.mesh.topology.D;
-        off=p.mesh.topology.offset["$(dim-1)$dim"];
-        inc=p.mesh.topology.incidence["$(dim-1)$dim"];
+function adaptGeometry!(m::mesh,pert::Tuple{Float64,Float64})
+    coord=m.geometry.coordinates;
+    xR=m.geometry.r[1]; xL=m.geometry.l[1]; yR=m.geometry.r[2]; yL=m.geometry.l[2]
+    dx=(xR-xL)/m.topology.n[1];
+    dy=(yR-yL)/m.topology.n[2];
 
-        if adaptBoundary==false
-            offe=p.mesh.topology.offset["$(dim-1)0"];
-            ince=p.mesh.topology.incidence["$(dim-1)0"];
-
-            bv=Set{Int64}()
-            b=Dict{Int64, Array{Int64,1}}();
-            for i in 1:(length(off)-1)
-                if off[i+1]-off[i]==1
-                    incv=ince[offe[i]:(offe[i+1]-1)];
-                    for j in incv
-                        push!(bv, j);
-                    end
-                    fi=inc[off[i]];
-                    if haskey(b, fi)
-                        push!(b[fi],i);
-                    else
-                        b[fi]=[i];
-                    end
-                end
-            end
-            for i in 1:size(coord,2)
-                if !in(i,bv)
-                    x=r*rand([-1,1])*rand(1)[1];
-                    y=rand([-1,1])*sqrt(r*r-x*x)*rand(1)[1];
-                    coord[:,i]=[coord[1,i]+x, coord[2,i]+y];
-                end
-            end
-        else
-            b=getBoundary(p.mesh);
-            for i in 1:size(coord,2)
-                x=r*rand([-1,1])*rand(1)[1];
-                y=rand([-1,1])*sqrt(r*r-x*x)*rand(1)[1];
-                coord[:,i]=[coord[1,i]+x, coord[2,i]+y];
-            end
-        end
-
-        for k in keys(p.degF)
-            p.degF[k]=degF(p.mesh, k, b, p.boundary,p.kubPoints);
-        end
-        if !isempty(p.massM)
-                assembMass!(p);
-        end
-        if !isempty(p.stiffM)
-                assembStiff!(p);
-        end
-        return nothing;
+    for i in 1:size(coord,2)
+        x=(coord[1,i]);
+        y=(coord[2,i]);
+        hx=coord[1,i]+pert[1]*sin(2*pi*(y-yL)/(yR-yL))*dx;
+        hy=coord[2,i]+pert[2]*sin(2*pi*(x-xL)/(xR-xL))*dy;
+        coord[:,i]=[hx, hy];
+    end
+    return nothing;
 end
 
-function adaptGeometry!(p::femProblem,pertX::Float64, pertY::Float64,adaptBoundary::Bool)
-        coord=p.mesh.geometry.coordinates;
-        dim=p.mesh.topology.D;
-        off=p.mesh.topology.offset["$(dim-1)$dim"];
-        inc=p.mesh.topology.incidence["$(dim-1)$dim"];
-        xR=p.mesh.geometry.r[1]; xL=p.mesh.geometry.l[1]; yR=p.mesh.geometry.r[2]; yL=p.mesh.geometry.l[2]
-        dx=(xR-xL)/p.mesh.topology.n[1];
-        dy=(yR-yL)/p.mesh.topology.n[2];
+function adaptGeometry!(m::mesh,pert::Tuple{Float64,Float64},bv::Set{Int64})
+    coord=m.geometry.coordinates;
+    xR=m.geometry.r[1]; xL=m.geometry.l[1]; yR=m.geometry.r[2]; yL=m.geometry.l[2]
+    dx=(xR-xL)/m.topology.n[1];
+    dy=(yR-yL)/m.topology.n[2];
 
-        if adaptBoundary==false
-            offe=p.mesh.topology.offset["$(dim-1)0"];
-            ince=p.mesh.topology.incidence["$(dim-1)0"];
-
-            bv=Set{Int64}()
-            b=Dict{Int64, Array{Int64,1}}();
-            for i in 1:(length(off)-1)
-                if off[i+1]-off[i]==1
-                    incv=ince[offe[i]:(offe[i+1]-1)];
-                    for j in incv
-                        push!(bv, j);
-                    end
-                    fi=inc[off[i]];
-                    if haskey(b, fi)
-                        push!(b[fi],i);
-                    else
-                        b[fi]=[i];
-                    end
-                end
-            end
-            for i in 1:size(coord,2)
-                if !in(i,bv)
-                    x=(coord[1,i]);
-                    y=(coord[2,i]);
-                    hx=coord[1,i]+pertX*sin(2*pi*(y-yL)/(yR-yL))*dx;
-                    hy=coord[2,i]+pertY*sin(2*pi*(x-xL)/(xR-xL))*dy;
-                    coord[:,i]=[hx, hy];
-                end
-            end
-        else
-            b=getBoundary(p.mesh);
-            for i in 1:size(coord,2)
-                x=(coord[1,i]);
-                y=(coord[2,i]);
-                hx=coord[1,i]+pertX*sin(2*pi*(y-yL)/(yR-yL))*dx;
-                hy=coord[2,i]+pertY*sin(2*pi*(x-xL)/(xR-xL))*dy;
-                coord[:,i]=[hx, hy];
-            end
+    for i in 1:size(coord,2)
+        if !in(i,bv)
+            x=(coord[1,i]);
+            y=(coord[2,i]);
+            hx=coord[1,i]+pert[1]*sin(2*pi*(y-yL)/(yR-yL))*dx;
+            hy=coord[2,i]+pert[2]*sin(2*pi*(x-xL)/(xR-xL))*dy;
+            coord[:,i]=[hx, hy];
         end
-
-        for k in keys(p.degF)
-            p.degF[k]=degF(p.mesh, k, b, p.boundary,p.kubPoints);
-        end
-        if !isempty(p.massM)
-                assembMass!(p);
-        end
-        if !isempty(p.stiffM)
-                assembStiff!(p);
-        end
-        return nothing;
+    end
+    return nothing;
 end
