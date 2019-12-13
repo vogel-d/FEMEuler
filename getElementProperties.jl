e598e14bc65ab2848da3941b9005f68e3551045a
rug-levin <levinrug98@gmail.com>
2019-12-13 13:27:25 +0100

    added higher finite Element spaces P2/RT1

--- getElementProperties.jl
+++ getElementProperties.jl
@@ -12,8 +12,94 @@ function getQuadElementProperties(type::Symbol, kubPoints::Array{Float64,2})
     fym1(x,y)=y-1;
     fymxy(x,y)=y-x*y;
     fxmxy(x,y)=x-x*y;
+    fxymy(x,y)=x*y-y;
+    fxymx(x,y)=x*y-x;
     fxy(x,y)=x*y;
     fl(x,y)=1-x-y+x*y;
+    fml(x,y)=-(1-x-y+x*y);
+
+    f1P2(x,y)=((2*x-1)*(x-1))*((2*y-1)*(y-1));
+    f2P2(x,y)=(4*(x-x^2))*((2*y-1)*(y-1));
+    f3P2(x,y)=(2*x^2-x)*((2*y-1)*(y-1));
+    f4P2(x,y)=((2*x-1)*(x-1))*(4*(y-y^2));
+    f5P2(x,y)=(4*(x-x^2))*(4*(y-y^2));
+    f6P2(x,y)=(2*x^2-x)*(4*(y-y^2));
+    f7P2(x,y)=((2*x-1)*(x-1))*(2*y^2-y);
+    f8P2(x,y)=(4*(x-x^2))*(2*y^2-y);
+    f9P2(x,y)=(2*x^2-x)*(2*y^2-y);
+
+    fgrad11P2(x,y)=(-3+4*x)*(1-3*y+2*y^2);
+    fgrad12P2(x,y)=(1-3*x+2*x^2)*(-3+4*y);
+    fgrad21P2(x,y)=4*(1-2*x)*(-1+y)*(-1+2*y);
+    fgrad22P2(x,y)=-4*(-1+x)*x*(-3+4*y);
+    fgrad31P2(x,y)=(-1+4*x)*(-1+y)*(-1+2*y);
+    fgrad32P2(x,y)=x*(-1+2*x)*(-3+4*y);
+    fgrad41P2(x,y)=-4*(-3+4*x)*(-1+y)*y;
+    fgrad42P2(x,y)=4*(-1+x)*(-1+2*x)*(1-2*y);
+    fgrad51P2(x,y)=16*(-1+2*x)*(-1+y)*y;
+    fgrad52P2(x,y)=16*(-1+x)*x*(-1+2*y);
+    fgrad61P2(x,y)=-4*(-1+4*x)*(-1+y)*y;
+    fgrad62P2(x,y)=4*x*(-1+2*x)*(1-2*y);
+    fgrad71P2(x,y)=(-3+4*x)*y*(-1+2*y);
+    fgrad72P2(x,y)=(-1+x)*(-1+2*x)*(-1+4*y);
+    fgrad81P2(x,y)=4*(1-2*x)*y*(-1+2*y);
+    fgrad82P2(x,y)=-4*(-1+x)*x*(-1+4*y);
+    fgrad91P2(x,y)=(-1+4*x)*y*(-1+2*y);
+    fgrad92P2(x,y)=x*(-1+2*x)*(-1+4*y);
+
+
+    f1RT1(x,y)=(1-x)*(2*y-1)*(y-1);
+    f2RT1(x,y)=4*(1-y)*(x-x^2);
+    f3RT1(x,y)=x*(2*y-1)*(y-1);
+    f4RT1(x,y)=(1-y)*(2*x^2-x);
+    f5RT1(x,y)=4*x*(y-y^2);
+    f6RT1(x,y)=y*(2*x^2-x);
+    f7RT1(x,y)=x*(2*y^2-y);
+    f8RT1(x,y)=4*y*(x-x^2);
+    f9RT1(x,y)=(1-x)*(2*y^2-y);
+    f10RT1(x,y)=y*(2*x-1)*(x-1);
+    f11RT1(x,y)=4*(1-x)*(y-y^2);
+    f12RT1(x,y)=(1-y)*(2*x-1)*(x-1);
+
+    fdiv1RT1(x,y)=(1-x)*(4*y-3);
+    fdiv2RT1(x,y)=4*(1-y)*(1-2*x);
+    fdiv3RT1(x,y)=x*(4*y-3);
+    fdiv4RT1(x,y)=(1-y)*(4*x-1);
+    fdiv5RT1(x,y)=4*x*(1-2*y);
+    fdiv6RT1(x,y)=y*(4*x-1);
+    fdiv7RT1(x,y)=x*(4*y-1);
+    fdiv8RT1(x,y)=4*y*(1-2*x);
+    fdiv9RT1(x,y)=(1-x)*(4*y-1);
+    fdiv10RT1(x,y)=y*(4*x-3);
+    fdiv11RT1(x,y)=4*(1-x)*(1-2*y);
+    fdiv12RT1(x,y)=(1-y)*(4*x-3);
+
+
+    #Namen: fgrad-indexphi-indexingradphimatrix-RT1
+    fgrad121RT1(x,y)=-(2*y-1)*(y-1);
+    fgrad122RT1(x,y)=(1-x)*(4*y-3);
+    fgrad211RT1(x,y)=4*(1-y)*(1-2*x);
+    fgrad212RT1(x,y)=-4*(x-x^2);
+    fgrad321RT1(x,y)=(2*y-1)*(y-1);
+    fgrad322RT1(x,y)=x*(4*y-3);
+    fgrad411RT1(x,y)=(1-y)*(4*x-1);
+    fgrad412RT1(x,y)=-(2*x^2-x);
+    fgrad521RT1(x,y)=4*(y-y^2);
+    fgrad522RT1(x,y)=4*x*(1-2*y);
+    fgrad611RT1(x,y)=y*(4*x-1);
+    fgrad612RT1(x,y)=2*x^2-x;
+    fgrad721RT1(x,y)=2*y^2-y;
+    fgrad722RT1(x,y)=x*(4*y-1);
+    fgrad811RT1(x,y)=4*y*(1-2*x);
+    fgrad812RT1(x,y)=4*(x-x^2);
+    fgrad921RT1(x,y)=-(2*y^2-y);
+    fgrad922RT1(x,y)=(1-x)*(4*y-1);
+    fgrad1011RT1(x,y)=y*(4*x-3);
+    fgrad1012RT1(x,y)=(2*x-1)*(x-1);
+    fgrad1121RT1(x,y)=-4*(y-y^2);
+    fgrad1122RT1(x,y)=4*(1-x)*(1-2*y);
+    fgrad1211RT1(x,y)=(1-y)*(4*x-3);
+    fgrad1212RT1(x,y)=-(2*x-1)*(x-1);
 
     if type==:DG0
         phi=[f1];
@@ -96,6 +182,41 @@ function getQuadElementProperties(type::Symbol, kubPoints::Array{Float64,2})
         cm=[0.5 0.0 0.0 0.0; 1.0 0.5 0.0 0.0; 0.5 1.0 0.0 0.0; 0.0 0.5 0.0 0.0];
         comp=[0, 0];
         discontType= true;
+
+    elseif type==:P2
+        phi=[f1P2, f2P2, f3P2, f4P2, f5P2, f6P2, f7P2, f8P2, f9P2];
+        c=[0.0 0.5 1.0 0.0 0.5 1.0 0.0 0.5 1.0;
+           0.0 0.0 0.0 0.5 0.5 0.5 1.0 1.0 1.0];
+
+        divphi=[null null null null null null null null null];
+        gradphi=[fgrad11P2 fgrad21P2 fgrad31P2 fgrad41P2 fgrad51P2 fgrad61P2 fgrad71P2 fgrad81P2 fgrad91P2;
+                 fgrad12P2 fgrad22P2 fgrad32P2 fgrad42P2 fgrad52P2 fgrad62P2 fgrad72P2 fgrad82P2 fgrad92P2];
+
+        cm=[0.5 0.0 1.0 1.0 1.0 0.0 0.0 0.0 0.0 0.0 0.0;
+            1.0 0.5 0.0 0.0 1.0 0.0 0.0 1.0 0.0 0.0 1.0;
+            0.5 1.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0 1.0 1.0;
+            0.0 0.5 1.0 0.0 0.0 1.0 0.0 0.0 1.0 0.0 0.0];
+
+        comp=[0, 0, 0, 0, 0, 0, 0, 0, 0];
+        discontType= false;
+
+    elseif type==:DG2
+        phi=[f1P2, f2P2, f3P2, f4P2, f5P2, f6P2, f7P2, f8P2, f9P2];
+        c=[0.0 0.5 1.0 0.0 0.5 1.0 0.0 0.5 1.0;
+           0.0 0.0 0.0 0.5 0.5 0.5 1.0 1.0 1.0];
+
+        divphi=[null null null null null null null null null];
+        gradphi=[fgrad11P2 fgrad21P2 fgrad31P2 fgrad41P2 fgrad51P2 fgrad61P2 fgrad71P2 fgrad81P2 fgrad91P2;
+                 fgrad12P2 fgrad22P2 fgrad32P2 fgrad42P2 fgrad52P2 fgrad62P2 fgrad72P2 fgrad82P2 fgrad92P2];
+
+        cm=[0.5 0.0 1.0 1.0 1.0 0.0 0.0 0.0 0.0 0.0 0.0;
+            1.0 0.5 0.0 0.0 1.0 0.0 0.0 1.0 0.0 0.0 1.0;
+            0.5 1.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0 1.0 1.0;
+            0.0 0.5 1.0 0.0 0.0 1.0 0.0 0.0 1.0 0.0 0.0];
+
+        comp=[0, 0, 0, 0, 0, 0, 0, 0, 0];
+        discontType= true;
+
     elseif type==:RT0
 
         phi=[null fx null f1mx;
@@ -125,6 +246,50 @@ function getQuadElementProperties(type::Symbol, kubPoints::Array{Float64,2})
         comp=[2,1,2,1];
 
         discontType= true;
+
+
+
+    elseif type==:RT1
+
+        phi=[null f2RT1 null f4RT1 null f6RT1 null f8RT1 null f10RT1 null f12RT1;
+             f1RT1 null f3RT1 null f5RT1 null f7RT1 null f9RT1 null f11RT1 null]
+        c=[0.0 0.5 1.0 1.0 1.0 1.0 1.0 0.5 0.0 0.0 0.0 0.0;
+           0.0 0.0 0.0 0.0 0.5 1.0 1.0 1.0 1.0 1.0 0.5 0.0];
+
+        divphi=[fdiv1RT1, fdiv2RT1, fdiv3RT1, fdiv4RT1, fdiv5RT1, fdiv6RT1,
+                fdiv7RT1, fdiv8RT1, fdiv9RT1, fdiv10RT1, fdiv11RT1, fdiv12RT1];
+
+        gradphi=[null null fgrad211RT1 fgrad212RT1 null null fgrad411RT1 fgrad412RT1 null null fgrad611RT1 fgrad612RT1 null null fgrad811RT1 fgrad812RT1 null null fgrad1011RT1 fgrad1012RT1 null null fgrad1211RT1 fgrad1212RT1;
+                 fgrad121RT1 fgrad122RT1 null null fgrad321RT1 fgrad322RT1 null null fgrad521RT1 fgrad522RT1 null null fgrad721RT1 fgrad722RT1 null null fgrad921RT1 fgrad922RT1 null null fgrad1121RT1 fgrad1122RT1 null null];
+
+        cm=[0.5 0.0 1.0 0.0 1.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+            1.0 0.5 0.0 0.0 0.0 1.0 0.0 1.0 0.0 0.0 0.0 0.0 0.0 0.0;
+            0.5 1.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0 1.0 0.0 0.0 0.0;
+            0.0 0.5 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0 1.0];
+        comp=[2,1,2,1,2,1,2,1,2,1,2,1];
+
+        discontType=false;
+
+    elseif type==:RT1B
+
+        phi=[null f2RT1 null f4RT1 null f6RT1 null f8RT1 null f10RT1 null f12RT1;
+             f1RT1 null f3RT1 null f5RT1 null f7RT1 null f9RT1 null f11RT1 null]
+        c=[0.0 0.5 1.0 1.0 1.0 1.0 1.0 0.5 0.0 0.0 0.0 0.0;
+           0.0 0.0 0.0 0.0 0.5 1.0 1.0 1.0 1.0 1.0 0.5 0.0];
+
+        divphi=[fdiv1RT1, fdiv2RT1, fdiv3RT1, fdiv4RT1, fdiv5RT1, fdiv6RT1,
+                fdiv7RT1, fdiv8RT1, fdiv9RT1, fdiv10RT1, fdiv11RT1, fdiv12RT1];
+
+        gradphi=[null null fgrad211RT1 fgrad212RT1 null null fgrad411RT1 fgrad412RT1 null null fgrad611RT1 fgrad612RT1 null null fgrad811RT1 fgrad812RT1 null null fgrad1011RT1 fgrad1012RT1 null null fgrad1211RT1 fgrad1212RT1;
+                 fgrad121RT1 fgrad122RT1 null null fgrad321RT1 fgrad322RT1 null null fgrad521RT1 fgrad522RT1 null null fgrad721RT1 fgrad722RT1 null null fgrad921RT1 fgrad922RT1 null null fgrad1121RT1 fgrad1122RT1 null null];
+
+        cm=[0.5 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+            1.0 0.5 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+            0.5 1.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+            0.0 0.5 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0];
+        comp=[2,1,2,1,2,1,2,1,2,1,2,1];
+
+        discontType=true;
 
     elseif type==:VecP1
         phi=[null null fxmxy fxy null null fymxy fl;
--- getPhi.jl
+++ getPhi.jl
@@ -1,88 +1,87 @@
 function getPhi(type::Symbol)
+
+    null(x,y)=0.0;
+    f1(x,y)=1.0;
+    fm1(x,y)=-1.0;
+    fx(x,y)=x;
+    fmx(x,y)=-x;
+    fy(x,y)=y;
+    fmy(x,y)=-y;
+    f1mx(x,y)=1-x;
+    fxm1(x,y)=x-1;
+    f1my(x,y)=1-y;
+    fym1(x,y)=y-1;
+    fymxy(x,y)=y-x*y;
+    fxmxy(x,y)=x-x*y;
+    fxymy(x,y)=x*y-y;
+    fxymx(x,y)=x*y-x;
+    fxy(x,y)=x*y;
+    fl(x,y)=1-x-y+x*y;
+    fml(x,y)=-(1-x-y+x*y);
+
+    f1P2(x,y)=((2*x-1)*(x-1))*((2*y-1)*(y-1));
+    f2P2(x,y)=(4*(x-x^2))*((2*y-1)*(y-1));
+    f3P2(x,y)=(2*x^2-x)*((2*y-1)*(y-1));
+    f4P2(x,y)=((2*x-1)*(x-1))*(4*(y-y^2));
+    f5P2(x,y)=(4*(x-x^2))*(4*(y-y^2));
+    f6P2(x,y)=(2*x^2-x)*(4*(y-y^2));
+    f7P2(x,y)=((2*x-1)*(x-1))*(2*y^2-y);
+    f8P2(x,y)=(4*(x-x^2))*(2*y^2-y);
+    f9P2(x,y)=(2*x^2-x)*(2*y^2-y);
+
+    f1RT1(x,y)=(1-x)*(2*y-1)*(y-1);
+    f2RT1(x,y)=4*(1-y)*(x-x^2);
+    f3RT1(x,y)=x*(2*y-1)*(y-1);
+    f4RT1(x,y)=(1-y)*(2*x^2-x);
+    f5RT1(x,y)=4*x*(y-y^2);
+    f6RT1(x,y)=y*(2*x^2-x);
+    f7RT1(x,y)=x*(2*y^2-y);
+    f8RT1(x,y)=4*y*(x-x^2);
+    f9RT1(x,y)=(1-x)*(2*y^2-y);
+    f10RT1(x,y)=y*(2*x-1)*(x-1);
+    f11RT1(x,y)=4*(1-x)*(y-y^2);
+    f12RT1(x,y)=(1-y)*(2*x-1)*(x-1);
+
+
     if type==:DG0
-        phi1(x,y)=1.0;
-        phi=[phi1];
+        phi=[f1];
         psize=(1,1);
 
-    elseif type==:P1
-        phip1(x,y)=(1-x-y+x*y); #(0,0)
-        phip2(x,y)=(x-x*y); #(1,0)
-        phip3(x,y)=(y-x*y); #(0,1)
-        phip4(x,y)=(x*y); #(1,1)
-
-        phi=[phip1,phip2,phip3,phip4];
+    elseif type==:P1 || type==:DG1
+        phi=[fl,fxmxy,fymxy,fxy];
         psize=(1,4);
 
     elseif type==:P1x || type==:DG1x
-        phipx1(x,y)=(1-x);
-        phipx2(x,y)=(x);
-        phi=[phipx1,phipx2];
+        phi=[f1mx,fx];
         psize=(1,2);
 
     elseif type==:P1y || type==:DG1y
-        phipy1(x,y)=(1-y);
-        phipy2(x,y)=(y);
-        phi=[phipy1,phipy2];
+        phi=[f1my,fy];
         psize=(1,2);
 
-    elseif type==:DG1
-        phid1(x,y)=(1-x-y+x*y); #(0,0)
-        phid2(x,y)=(x-x*y); #(1,0)
-        phid3(x,y)=(y-x*y); #(0,1)
-        phid4(x,y)=(x*y); #(1,1)
+    elseif type==:P2 || type==:DG2
+        phi=[f1P2, f2P2, f3P2, f4P2, f5P2, f6P2, f7P2, f8P2, f9P2];
+        psize=(1,9)
 
-        phi=[phid1,phid2,phid3,phid4];
-        psize=(1,4);
-
-    elseif type==:RT0
-        phiv1(x,y)=x;
-        phiv0(x,y)=0;
-        phiv2(x,y)=(1-x);
-        phiv3(x,y)=y;
-        phiv4(x,y)=(1-y);
-
-        phi=[phiv0 phiv1 phiv0 phiv2;
-            phiv4 phiv0 phiv3 phiv0];
+    elseif type==:RT0 || type==:RT0B
+        phi=[null fx null f1mx;
+             f1my null fy null];
         #phi=[phiv0 phiv4; phiv1 phiv0; phiv0 phiv3; phiv2 phiv0];
         psize=(2,4);
 
-    elseif type==:RT0B
-        phiv1d(x,y)=x;
-        phiv0d(x,y)=0;
-        phiv2d(x,y)=(1-x);
-        phiv3d(x,y)=y;
-        phiv4d(x,y)=(1-y);
+    elseif type==:RT1 || type==:RT1B
+        phi=[null f2RT1 null f4RT1 null f6RT1 null f8RT1 null f10RT1 null f12RT1;
+             f1RT1 null f3RT1 null f5RT1 null f7RT1 null f9RT1 null f11RT1 null];
+        psize=(2,12);
 
-        phi=[phiv0d phiv1d phiv0d phiv2d;
-             phiv4d phiv0d phiv3d phiv0d];
-        #phi=[phiv0d phi4d; phiv1d phiv0d; phiv0d phiv3d; phiv2d phiv0d]';
-        psize=(2,4);
-
-    elseif type==:VecP1
-        phiw0(x,y)=0;
-        phiw1(x,y)=(1-x)*(1-y);
-        phiw2(x,y)=x*(1-y);
-        phiw3(x,y)=x*y;
-        phiw4(x,y)=(1-x)*y;
-
-        phi=[phiw0 phiw0 phiw2 phiw3 phiw0 phiw0 phiw4 phiw1;
-             phiw1 phiw2 phiw0 phiw0 phiw3 phiw4 phiw0 phiw0];
+    elseif type==:VecP1 || type==:VecDG1
+        phi=[null null fxmxy fxy null null fymxy fl;
+             fl fxmxy null null fxy fymxy null null];
         #phi=[phiw0 phiw1; phiw0 phiw2; phiw2 phiw0; phiw3 phiw0; phiw0 phiw3; phiw0  phiw4; phiw4 phiw0;phiw1 phiw0]
 
         psize=(2,8);
-
-    elseif type==:VecDG1
-        phiw0d(x,y)=0;
-        phiw1d(x,y)=(1-x)*(1-y);
-        phiw2d(x,y)=x*(1-y);
-        phiw3d(x,y)=x*y;
-        phiw4d(x,y)=(1-x)*y;
-
-        phi=[phiw0d phiw0d phiw2d phiw3d phiw0d phiw0d phiw4d phiw1d;
-             phiw1d phiw2d phiw0d phiw0d phiw3d phiw4d phiw0d phiw0d];
-        #phi=[phiw0d phiw1d; phiw0d phiw2d; phiw2d phiw0d; phiw3d phiw0d; phiw0d phiw3d; phiw0d  phiw4d; phiw4d phiw0d;phiw1d phiw0d]
-
-        psize=(2,8);
+    else
+        error("Ansatzfunktionen zu finitem Element $type nicht gefunden.")
     end
 
     return phi, psize;
