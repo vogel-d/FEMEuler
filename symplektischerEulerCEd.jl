e0cc4765b869cc102f03b956af0b753b1f995ef9
vogel-d <denise.vogel@gmx.net>
2020-01-17 18:06:15 +0100

    decreased allocations

--- symplektischerEulerCEd.jl
+++ symplektischerEulerCEd.jl
@@ -1,14 +1,15 @@
-function symplektischerEuler!(y::solution,p::femProblem,w::solution,Sth::SparseMatrixCSC{Float64,Int64},ns::Int64,dtau::Float64)
+function symplektischerEuler!(y::solution,p::femProblem,w::solution,Sth::SparseMatrixCSC{Float64,Int64},ns::Int64,dtau::Float64,
+                              velOld::Array{Float64,1},rhoVS::Array{Float64,1},rhoS::Array{Float64,1},rhoThetaS::Array{Float64,1})
   gamma=0.01;
   numRho=p.degFBoundary[p.femType[:rho][1]].num
   numRhoV=p.degFBoundary[p.femType[:rhoV][1]].num
   numRhoTheta=p.degFBoundary[p.femType[:rhoTheta][1]].num
 
   for i in 1:ns
-    velOld=copy(y.rhoV);
-    rhoVS=velocity(p,y.rho,y.rhoTheta);
+    copy!(velOld,y.rhoV);
+    velocity!(rhoVS,p,y.rho,y.rhoTheta);
     @. y.rhoV[1:numRhoV]+=dtau*(rhoVS+w.rhoV[1:numRhoV]);
-    rhoS,rhoThetaS=rhoTheta(p,(1+gamma)*y.rhoV-gamma*velOld, Sth);
+    rhoTheta!(rhoS,rhoThetaS,p,(1+gamma)*y.rhoV-gamma*velOld, Sth);
     @. y.rho[1:numRho]+=dtau*(rhoS+w.rho[1:numRho]);
     @. y.rhoTheta[1:numRhoTheta]+=dtau*(rhoThetaS+w.rhoTheta[1:numRhoTheta]);
   end
@@ -16,12 +17,12 @@ function symplektischerEuler!(y::solution,p::femProblem,w::solution,Sth::SparseM
   return nothing;
 end
 
-function velocity(p::femProblem, yRho::Array{Float64,1},yRhoTheta::Array{Float64,1})
+function velocity!(rhoVS::Array{Float64,1},p::femProblem, yRho::Array{Float64,1},yRhoTheta::Array{Float64,1})
   pres=projectPressure(p.degFBoundary[p.femType[:p][1]],p.massMBoundary[p.femType[:p][1]],p.degFBoundary[p.femType[:rhoTheta][1]],yRhoTheta,p.mesh,p.kubPoints,p.kubWeights);
   return p.massM[p.femType[:rhoV][1]]\(-(p.stiffM[:vp]*(pres-p.diagnostic.pBar)+9.81*(p.stiffM[:vrho]*(yRho-p.diagnostic.rhoBar))));
 end
 
-function rhoTheta(p::femProblem, yRhoV::Array{Float64,1}, STh::SparseMatrixCSC{Float64,Int64})
+function rhoTheta!(rhoS::Array{Float64,1},rhoThetaS::Array{Float64,1},p::femProblem, yRhoV::Array{Float64,1}, STh::SparseMatrixCSC{Float64,Int64})
   rhoS=p.massM[p.femType[:rho][1]]\(-p.stiffM[:rho]*yRhoV);
   rhoThetaS=p.massM[p.femType[:rhoTheta][1]]\(STh*yRhoV);
   return rhoS, rhoThetaS;
