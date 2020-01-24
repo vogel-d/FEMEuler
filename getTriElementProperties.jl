b9cc000dd93a43823503f16196151f227f62ecf6
rug-levin <levinrug98@gmail.com>
2020-01-24 09:41:22 +0100

    corrected to work for triangles (still not working)

--- getTriElementProperties.jl
+++ getTriElementProperties.jl
@@ -1,18 +1,31 @@
 function getTriElementProperties(type::Symbol)
     null(x,y)=0.0;
     f1(x,y)=1.0;
-    f1s(x,y)=1.0/sqrt(2);
+    fm1(x,y)=-1.0;
     f2(x,y)=2.0;
-    f2s(x,y)=2.0/sqrt(2);
     fx(x,y)=x;
     fmx(x,y)=-x;
+    
+    f1s(x,y)=1.0/sqrt(2);
+    f2s(x,y)=2.0/sqrt(2);
     fsx(x,y)=(0.7071067811865475244)*x;
+    fsy(x,y)=(0.7071067811865475244)*y;
+
+    #=
+    f1s(x,y)=1.4142135623730951;
+    f2s(x,y)=2.0*1.4142135623730951;
+    fsx(x,y)=1.4142135623730951*x;
+    fsy(x,y)=1.4142135623730951*y;
+    =#
     fy(x,y)=y;
     fmy(x,y)=-y;
-    fsy(x,y)=(0.7071067811865475244)*y;
     fxm1(x,y)=x-1;
     fym1(x,y)=y-1;
     f1mxy(x,y)=1-x*y;
+    f1mxmy(x,y)=1-x-y;
+    f1mx(x,y)=1-x;
+    f1my(x,y)=1-y;
+
 
 
     if type==:DG0
@@ -31,32 +44,38 @@ function getTriElementProperties(type::Symbol)
 
     elseif type==:RT0
 
-        phi=[fxm1 fx fsx;
-             fy fym1 fsy];
+        #phi=[fxm1 fx fsx;
+        #     fy fym1 fsy];
+         phi=[fx fsx fxm1;
+              fym1 fsy fy];
         #c=[0.5 0.0 0.5; 0.0 0.5 0.5];
 
-        divphi=[f2, f2, f2s];
-        gradphi=[f1 null f1 null f1s null;
-                 null f1 null f1 null f1s];
+        #divphi=[f2, f2, f2s];
+        #gradphi=[f1 null f1 null f1s null;
+        #         null f1 null f1 null f1s];
+
+        divphi=[f2, f2s, f2];
+        gradphi=[f1 null f1s null f1 null;
+                 null f1 null f1s null f1];
 
         nFace=0;
         nEdge=1;
         nVert=0;
 
-        cm=[0.5 0.0 0.0 1.0 0.0;
-            0.5 0.5 0.0 0.0 1.0;
-            0.0 0.5 1.0 0.0 0.0];
+        cm=[0.5 0.0 1.0 0.0 0.0;
+            0.5 0.5 0.0 1.0 0.0;
+            0.0 0.5 0.0 0.0 1.0];
 
 
 
     elseif type==:P1
-        phi=[f1mxy, fx, fy];
+        phi=[f1mxmy, fx, fy];
         #c=[0.0 1.0 0.0; 0.0 0.0 1.0];
 
         divphi=[null, null, null];
 
-        gradphi=[fmy f1 null;
-                 fmx null f1];
+        gradphi=[fm1 f1 null;
+                 fm1 null f1];
 
         nFace=0;
         nEdge=0;
--- jacobi.jl
+++ jacobi.jl
@@ -19,12 +19,14 @@ function jacobi!(J::Array{Array{Float64,2},2},dJ::Array{Float64,2},m::mesh, fid:
     if mt==3
         a=coord[:,2]-coord[:,1];
         b=coord[:,3]-coord[:,1];
-        one=ones(1,sk);
-        dJ=abs(a[1]*b[2]-b[1]*a[2])*one;
-        J[1,1]=a[1]*one;
-        J[1,2]=b[1]*one;
-        J[2,1]=a[2]*one;
-        J[2,2]=b[2]*one;
+        for i in 1:sk
+            dJ[i]=abs(a[1]*b[2]-b[1]*a[2]);
+
+            J[1,1][i]=a[1];
+            J[1,2][i]=b[1];
+            J[2,1][i]=a[2];
+            J[2,2][i]=b[2];
+        end
     elseif mt==4
          #J[1,1]=Array{Float64,2}(undef,sk,sk);
          #J[1,2]=Array{Float64,2}(undef,sk,sk);
@@ -82,33 +84,27 @@ function jacobi!(J::Array{Array{Float64,2},2},ddJ::Array{Float64,2},jphi::Array{
         end
         z+=1;
     end
-
+    
     sk=size(kubPoints,2);
 
     if mt==3
         a=coord[:,2]-coord[:,1];
         b=coord[:,3]-coord[:,1];
-        one=ones(1,sk);
-        dJ=(1/abs(a[1]*b[2]-b[1]*a[2]))*one;
-        J[1,1]=a[1]*one;
-        J[1,2]=b[1]*one;
-        J[2,1]=a[2]*one;
-        J[2,2]=b[2]*one;
-        #= alt
-        one=ones(sk,sk);
-        ddJ=1/abs(a[1]*b[2]-b[1]*a[2])*one;
-        J[1,1]=a[1]*one;
-        J[1,2]=b[1]*one;
-        J[2,1]=a[2]*one;
-        J[2,2]=b[2]*one;
-        =#
+        for i in 1:sk
+            ddJ[i]=1/abs(a[1]*b[2]-b[1]*a[2]);
 
-        for i=1:sk
+            J[1,1][1,i]=a[1];
+            J[1,2][1,i]=b[1];
+            J[2,1][1,i]=a[2];
+            J[2,2][1,i]=b[2];
+
             for k in 1:size(phi,2)
                 jphi[1,k][1,i]=(J[1,1][1,i]*phi[1,k][1,i]+J[1,2][1,i]*phi[2,k][1,i]);
                 jphi[2,k][1,i]=(J[2,1][1,i]*phi[1,k][1,i]+J[2,2][1,i]*phi[2,k][1,i]);
             end
         end
+        println("coord: $coord")
+        println([a[1] b[1];a[2] b[2]])
     elseif mt==4
         #J[1,1]=Array{Float64,2}(undef,sk,sk);
         #J[1,2]=Array{Float64,2}(undef,sk,sk);
@@ -141,20 +137,22 @@ function jacobi!(ddJ::Array{Float64,2}, jphi::Array{Array{Float64,2},2}, m::mesh
         end
         z+=1;
     end
+
     sk=size(kubPoints,2);
 
     if mt==3
         a=coord[:,2]-coord[:,1];
         b=coord[:,3]-coord[:,1];
-        one=ones(sk,sk)
-        ddJ=1/abs(a[1]*b[2]-b[1]*a[2])*one;
-        JtJ11=(a[1]^2+a[2]^2)*one;
-        JtJ12=(a[1]*b[1]+a[2]*b[2])*one;
-        JtJ2=(a[1]*b[1]+a[2]*b[2])*one;
-        JtJ22=(b[1]^2+b[2]^2)*one;
-        for k in 1:size(phi,2)
-            jphi[1,k][i,j]=(JtJ11*phi[1,k][i,j]+JtJ12*phi[2,k][i,j]);
-            jphi[2,k][i,j]=(JtJ21*phi[1,k][i,j]+JtJ22*phi[2,k][i,j]);
+        for i in 1:sk
+            ddJ[i]=1/abs(a[1]*b[2]-b[1]*a[2]);
+            JtJ11[i]=a[1]^2+a[2]^2;
+            JtJ12[i]=a[1]*b[1]+a[2]*b[2];
+            JtJ21[i]=a[1]*b[1]+a[2]*b[2];
+            JtJ22[i]=b[1]^2+b[2]^2;
+            for k in 1:size(phi,2)
+                jphi[1,k][i]=(JtJ11*phi[1,k][i]+JtJ12*phi[2,k][i]);
+                jphi[2,k][i]=(JtJ21*phi[1,k][i]+JtJ22*phi[2,k][i]);
+            end
         end
     elseif mt==4
         for i=1:sk, j=1:sk
@@ -195,12 +193,23 @@ function jacobi!(J::Array{Array{Float64,2},2},ddJ::Array{Float64,2},jphi::Array{
     if mt==3
         a=coord[:,2]-coord[:,1];
         b=coord[:,3]-coord[:,1];
-        one=ones(sk,sk);
-        ddJ=1/abs(a[1]*b[2]-b[1]*a[2])*one;
-        J[1,1]=a[1]*one;
-        J[1,2]=b[1]*one;
-        J[2,1]=a[2]*one;
-        J[2,2]=b[2]*one;
+        for i in 1:sk
+            ddJ[i]=1/abs(a[1]*b[2]-b[1]*a[2]);
+
+            J[1,1][i]=a[1];
+            J[1,2][i]=b[1];
+            J[2,1][i]=a[2];
+            J[2,2][i]=b[2];
+
+            for k in 1:size(phi,2)
+                jphi[1,k][i]=(J[1,1][i]*phi[1,k][i]+J[1,2][i]*phi[2,k][i]);
+                jphi[2,k][i]=(J[2,1][i]*phi[1,k][i]+J[2,2][i]*phi[2,k][i]);
+            end
+            for k in 1:size(psi,2)
+                jpsi[1,k][i]=(J[1,1][i]*psi[1,k][i]+J[1,2][i]*psi[2,k][i]);
+                jpsi[2,k][i]=(J[2,1][i]*psi[1,k][i]+J[2,2][i]*psi[2,k][i]);
+            end
+        end
     elseif mt==4
         #J[1,1]=Array{Float64,2}(undef,sk,sk);
         #J[1,2]=Array{Float64,2}(undef,sk,sk);
@@ -243,12 +252,13 @@ function jacobi!(J::Array{Array{Float64,1},2},dJ::Array{Float64,1},m::mesh, fid:
     if mt==3
         a=coord[:,2]-coord[:,1];
         b=coord[:,3]-coord[:,1];
-        one=ones(sk,sk);
-        dJ=abs(a[1]*b[2]-b[1]*a[2])*one;
-        J[1,1]=a[1]*one;
-        J[1,2]=b[1]*one;
-        J[2,1]=a[2]*one;
-        J[2,2]=b[2]*one;
+        for i in 1:sk
+            dJ[i]=abs(a[1]*b[2]-b[1]*a[2]);
+            J[1,1][i]=a[1];
+            J[1,2][i]=b[1];
+            J[2,1][i]=a[2];
+            J[2,2][i]=b[2];
+        end
     elseif mt==4
          #J[1,1]=Array{Float64,1}(undef,sk);
          #J[1,2]=Array{Float64,1}(undef,sk);
@@ -282,12 +292,22 @@ function jacobi!(J::Array{Array{Float64,1},2},ddJ::Array{Float64,1},jphi::Array{
     if mt==3
         a=coord[:,2]-coord[:,1];
         b=coord[:,3]-coord[:,1];
-        one=ones(sk);
-        ddJ=1/abs(a[1]*b[2]-b[1]*a[2])*one;
-        J[1,1]=a[1]*one;
-        J[1,2]=b[1]*one;
-        J[2,1]=a[2]*one;
-        J[2,2]=b[2]*one;
+        for i in 1:sk
+            ddJ[i]=1/abs(a[1]*b[2]-b[1]*a[2]);
+            J[1,1][i]=a[1];
+            J[1,2][i]=b[1];
+            J[2,1][i]=a[2];
+            J[2,2][i]=b[2];
+
+            for k in 1:size(phi,2)
+                jphi[1,k][i]=(J[1,1][i]*phi[1,k][i]+J[1,2][i]*phi[2,k][i]);
+                jphi[2,k][i]=(J[2,1][i]*phi[1,k][i]+J[2,2][i]*phi[2,k][i]);
+            end
+            for k in 1:size(psi,2)
+                jpsi[1,k][i]=(J[1,1][i]*psi[1,k][i]+J[1,2][i]*psi[2,k][i]);
+                jpsi[2,k][i]=(J[2,1][i]*psi[1,k][i]+J[2,2][i]*psi[2,k][i]);
+            end
+        end
     elseif mt==4
         #J[1,1]=Array{Float64,1}(undef,sk);
         #J[1,2]=Array{Float64,1}(undef,sk);
