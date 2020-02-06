c1180aa5a776cf290811b2a9b39630e658875fca
vogel-d <denise.vogel@gmx.net>
2020-02-06 20:15:39 +0100

    added Schaer mountain and Sleve coordinate transformation

--- adaptGeometry.jl
+++ adaptGeometry.jl
@@ -1,13 +1,31 @@
 function adaptGeometry!(m::mesh,hm::Float64,a::Float64)
     H=m.geometry.r[2];
-    h(x)=(hm*a^2)/(x^2+a^2); #Witch of Agnesi
+    xM=0.5(m.geometry.l[1]+m.geometry.r[1])
+    h(x)=(hm*a^2)/((x-xM)^2+a^2); #Witch of Agnesi
     coord=m.geometry.coordinates;
-    for i in 1:(m.topology.n[1]+1)
-        coord[2,i]=h(coord[1,i]);
-    end
-    for i in (m.topology.n[1]+2):size(coord,2)
+    for i in 1:size(coord,2)
         z0=h(coord[1,i])
         coord[2,i]=H*(coord[2,i]+z0)/(H+z0); #Gal-Chen & Sommerville transformation
+    end
+    return nothing;
+end
+
+function adaptGeometry!(m::mesh,hm::Float64,a::Float64,lambda::Float64)
+    H=m.geometry.r[2];
+    xM=0.5(m.geometry.l[1]+m.geometry.r[1])
+    function h(x) #Schaer
+        h2=hm*exp(-((x-xM)/a)^2);
+        h1=h2*cos(pi*(x-xM)/lambda)^2;
+        return h1, h2;
+    end
+    coord=m.geometry.coordinates;
+    sC=4000.0
+    sF=1000.0
+    nC=1.35
+    nF=1.35
+    for i in 1:size(coord,2)
+        zSF, zSC=h(coord[1,i])
+        coord[2,i]+=sinh((H/sC)^nC-(coord[2,i]/sC)^nC)/sinh((H/sC)^nC)*zSC+sinh((H/sF)^nF-(coord[2,i]/sF)^nF)/sinh((H/sF)^nF)*(zSF-zSC); #SLEVE
     end
     return nothing;
 end
