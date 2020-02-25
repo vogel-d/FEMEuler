540f86d6e08741a0062a30906ee70dda28d76dcb
rug-levin <levinrug98@gmail.com>
2020-02-25 10:41:32 +0100

    added advection testfiles

---
+++ symplektischerEulerA.jl
@@ -0,0 +1,48 @@
+function symplektischerEuler!(y::solution,p::femProblem, w::solution,ns::Float64,dtau::Float64,
+                              velOld::Array{Float64,1},vS::Array{Float64,1},pS::Array{Float64,1},bS::Array{Float64,1})
+
+  gamma=0.05;
+
+  np=p.degFBoundary[p.femType[:p][1]].num
+  nv=p.degFBoundary[p.femType[:v][1]].num
+  nb=p.degFBoundary[p.femType[:b][1]].num
+
+  for i in 1:ns
+    copy!(velOld,y.v);
+    #velocity!(vS,p,y.p,y.b)
+    #@. y.v[1:nv]+=dtau*(vS+w.v[1:nv]);
+    @. y.v[1:nv]+=dtau*(w.v[1:nv]);
+    #pressureBuoyancy!(pS,bS,p,(1+gamma)*y.v-gamma*velOld);
+    #@. y.p[1:np]+=dtau*(pS+w.p[1:np]);
+    @. y.p[1:np]+=dtau*(w.p[1:np]);
+    #@. y.b[1:nb]+=dtau*(bS+w.b[1:nb]);
+    @. y.b[1:nb]+=dtau*(w.b[1:nb]);
+
+  end
+
+  return nothing;
+end
+
+function velocity!(vS::Array{Float64,1},p::femProblem, yP::Array{Float64,1},yB::Array{Float64,1})
+  Fv=p.massM[p.femType[:v][1]];
+  Svp=p.stiffM[:vp];
+  Svb=p.stiffM[:vb];
+  vS[:]=Fv\(-Svp*yP+Svb*yB)
+  return nothing;
+end
+
+function pressureBuoyancy!(pS::Array{Float64,1},bS::Array{Float64,1},p::femProblem, yV::Array{Float64,1})
+  cs2=115600;
+  N2=1.e-4;
+
+  Fp=p.massM[p.femType[:p][1]];
+  Fb=p.massM[p.femType[:b][1]];
+
+  Spv=p.stiffM[:pv];
+  Sbv=p.stiffM[:bv];
+
+  pS[:]=Fp\(-cs2*(Spv*yV));
+  bS[:]=Fb\(-N2*(Sbv*yV));
+
+  return nothing;
+end
---
+++ testAdvectionTriangles.jl
@@ -0,0 +1,70 @@
+include("modulesBA.jl")
+include("symplektischerEulerA.jl")
+
+function testAdvectionTriangles()
+  filename = "test"
+
+  #order: comp, compHigh, compRec, compDG
+  femType=Dict(:p=>[:DG0, :P1, :DG1, :DG0], :v=>[:RT0, :VecP1, :VecDG1, :RT0B], :b=>[:DG0, :P1, :DG1, :DG0]);
+  #femType=Dict(:p=>[:DG0, :P1, :DG1, :DG0], :v=>[:RT0, :VecP1, :VecDG1, :RT0B], :b=>[:P1, :P1, :DG1, :DG1]);
+  #femType=Dict(:p=>[:DG1, :P1, :DG1, :DG0], :v=>[:RT1, :VecP1, :VecDG1, :RT0B], :b=>[:DG1, :P1, :DG1, :DG0]);
+  Vfcomp=:RT0
+  #Vfcomp=:RT1
+
+  taskRecovery=false;
+
+  m=generateTriMesh(300,20,:periodic,:constant,0.0,300000.0,0.0,10000.0); #(east/west, top/bottom)
+  #adaptGeometry!(m,(0.3,0.3),false); #sin perbutation
+  p=femProblem(m, femType, t=:boussinesq, taskRecovery=taskRecovery);
+
+  gamma=0.5;
+  UMax=20.0; #UMax determines the advection in x direction
+  #UMax=0.0;
+  MISMethod=MIS(:MIS4_4);
+
+  dt=20.0;
+  #dt=10.0;
+  ns=19;
+  EndTime=100.0;
+  nIter=Int64(EndTime/dt);
+
+  #start function
+  xR=m.geometry.r[1]; xL=m.geometry.l[1]; yR=m.geometry.r[2]; yL=m.geometry.l[2]
+  b0=0.01; H=10000; A=5000;
+  xM=0.5*(xL+xR);
+  fb(x,y)=b0*sin(pi*y/H)/(1+((x-xM)/A)^2);
+  f=Dict(:b=>fb)
+
+  assembMass!(p);
+  assembStiff!(p);
+  applyStartValues!(p, f);
+
+  v1(x,y)=UMax
+  v2(x,y)=0
+  V=[v1, v2];
+  Vf=projectAdvection(p,V,Vfcomp);
+
+  taskRecovery ? pos=[1,3] : pos=[1];
+  advectionTypes=Symbol[Vfcomp];
+  for i in keys(femType)
+      append!(advectionTypes,femType[i][pos]);
+  end
+  nquadPhi, nquadPoints=coordTrans(m.meshType, m.normals, collect(Set(advectionTypes)), size(p.kubWeights,2));
+  setEdgeData!(p, :v);
+
+  y=p.solution[0.0];
+  Time=0.0;
+  for i=1:nIter
+    y=splitExplicit(p,gamma,Vfcomp,Vf,nquadPhi,nquadPoints,MISMethod,y,Time,dt,ns);
+    Time=Time+dt
+    p.solution[Time]=y;
+    println(Time)
+  end
+
+  #Speichern des Endzeitpunktes als vtu-Datei:
+  #unstructured_vtk(p, EndTime, [:p, :b, :v], ["Pressure", "Buoyancy", "Velocity"], "testAdvectionTriangles/"*filename)
+  #Speichern aller berechneten Zwischenwerte als vtz-Datei:
+  unstructured_vtk(p, sort(collect(keys(p.solution))), [:p, :b, :v], ["Pressure", "Buoyancy", "Velocity"], "testAdvectionTriangles/"*filename)
+
+  return p
+end
