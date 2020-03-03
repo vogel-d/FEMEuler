e245b7b1f89aeb29767d5b79fcadccab3e4705b5
rug-levin <levinrug98@gmail.com>
2020-03-03 17:20:58 +0100

    splitted triangle/rect mesh (and changed n to n*length(edge))

--- getEdgeType.jl
+++ getEdgeType.jl
@@ -1,17 +1,30 @@
-function getEdgeType(n::Array{Float64,1})
-    if n==[0.0,-1.0]
-        edgeType=1;
-    elseif n==[-1.0,0.0]
-        edgeType=2;
-    elseif n==[0.0, 1.0]
-        edgeType=3;
-    elseif n==[1.0,0.0]
-        edgeType=4;
-    elseif isequal(n,[1/sqrt(2), 1/sqrt(2)])
-        edgeType=3;
-    else
-        error("Kein zulässiger Normalenvektor: $(n)");
+function getEdgeType(mt::Int, n::Array{Float64,1})
+    if mt==3
+        if n==[0.0,1.0]
+            edgeType=1;
+        elseif n==[0.0,-1.0]
+            edgeType=1;
+        elseif n==[1.0,1.0]
+            edgeType=2;
+        elseif n==[1.0,0.0]
+            edgeType=3;
+        elseif n==[-1.0,0.0]
+            edgeType=3;
+        else
+            error("Kein zulässiger Normalenvektor: $(n)");
+        end
+    elseif mt==4
+        if n==[0.0,-1.0]
+            edgeType=1;
+        elseif n==[-1.0,0.0]
+            edgeType=2;
+        elseif n==[0.0, 1.0]
+            edgeType=3;
+        elseif n==[1.0,0.0]
+            edgeType=4;
+        else
+            error("Kein zulässiger Normalenvektor: $(n)");
+        end
     end
-
     return edgeType
 end
