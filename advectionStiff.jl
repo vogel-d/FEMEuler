62f36cc5fd1d0516e2032554289a43805b7fb7e5
rug-levin <levinrug98@gmail.com>
2020-03-03 17:03:19 +0100

    added jacobi-routines to detect det(J)

--- advectionStiff.jl
+++ advectionStiff.jl
@@ -1,5 +1,5 @@
 function advectionStiff(degFT::degF{1}, phiTtrans::Array{Array{Array{Float64,1},2},1},
-                        degFF::degF{2}, phiFtrans::Array{Array{Array{Float64,1},2},1},  fval::SparseVector{Float64,Int64},
+                        degFF::degF{2}, phiFtrans::Array{Array{Array{Float64,1},2},1}, fval::SparseVector{Float64,Int64},
                         degFW::degF{1}, phiWtrans::Array{Array{Array{Float64,1},2},1}, wval::Array{Float64,1},
                         gamma::Float64,m::mesh, kubPoints::Array{Float64,2}, kubWeights::Array{Float64,2},
                         nquadPoints::Array{Array{Float64,2},1}, edgeData::Array{Array{Int64,1},1})
@@ -21,16 +21,26 @@ function advectionStiff(degFT::degF{1}, phiTtrans::Array{Array{Array{Float64,1},
     globalNumW2=Array{Int64,1}(undef,length(phiW));
 
     M=zeros(degFT.numB,1);
+    coord=Array{Float64,2}(undef,2,m.meshType);
+
+
+    #global St=zeros(degFT.numB,degFF.numB)
 
     discGalerkinCells!(M,degFT,phiT, globalNumT1, degFF,phiF, dphiF, fval, globalNumF1,
                        degFW, phiW, gradphiW, wval, globalNumW1,
-                       m, kubPoints, kubWeights)
+                       m, kubPoints, kubWeights, coord)
 
-    quadPoints, quadWeights=getQuad(2*sk[1]-1);
+    if sk[1]==1
+        quadPoints, quadWeights=getQuad(sk[2]+1); #TODO: find better way to handle g (argument of getQuad)
+    else
+        quadPoints, quadWeights=getQuad(2*sk[1]-1);
+    end
+
     discGalerkinEdges!(M,degFT,phiT, phiTtrans,globalNumT1, globalNumT2,
                        degFF,phiF, phiFtrans, fval, globalNumF1, globalNumF2,
                        degFW,phiW, phiWtrans, wval, globalNumW1,globalNumW2,
-                       m, quadWeights, nquadPoints, edgeData,gamma)
+                       m, quadWeights, nquadPoints, edgeData, gamma)
+
     return M[1:degFT.num]
 end
 
@@ -40,7 +50,6 @@ function advectionStiff(degFT::degF{2}, phiTtrans::Array{Array{Array{Float64,1},
                         gamma::Float64,m::mesh, kubPoints::Array{Float64,2}, kubWeights::Array{Float64,2},
                         nquadPoints::Array{Array{Float64,2},1}, edgeData::Array{Array{Int64,1},1})
 
-
     phiT=@views degFT.phi;
     phiF=@views degFF.phi;
     dphiF=@views degFF.divphi;
@@ -49,7 +58,13 @@ function advectionStiff(degFT::degF{2}, phiTtrans::Array{Array{Array{Float64,1},
 
     sk=size(kubWeights);
 
-    quadPoints, quadWeights=getQuad(2*sk[1]-1);
+    if sk[1]==1
+        quadPoints, quadWeights=getQuad(sk[2]+1); #TODO: find better way to handle g (argument of getQuad)
+    else
+        quadPoints, quadWeights=getQuad(2*sk[1]-1);
+    end
+
+
     coord=Array{Float64,2}(undef,2,m.meshType);
 
     globalNumT1=Array{Int64,1}(undef,size(phiT,2));
@@ -68,7 +83,7 @@ function advectionStiff(degFT::degF{2}, phiTtrans::Array{Array{Array{Float64,1},
     discGalerkinEdges!(M,degFT,phiT, phiTtrans,globalNumT1, globalNumT2,
                        degFF,phiF, phiFtrans, fval, globalNumF1, globalNumF2,
                        degFW,phiW, phiWtrans, wval, globalNumW1,globalNumW2,
-                       m, quadWeights, nquadPoints, edgeData,gamma,coord)
+                       m, quadWeights, nquadPoints, edgeData, gamma, coord)
 
     return M[1:degFT.num];
 end
