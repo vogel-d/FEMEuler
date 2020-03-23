ec707dcce8c0a8cd26fd332c849117bb62700c01
rug-levin <levinrug98@gmail.com>
2020-03-23 17:48:19 +0100

    using advection-only RK

--- testAdvectionTriangles.jl
+++ testAdvectionTriangles.jl
@@ -1,8 +1,7 @@
 include("modulesBA.jl")
-include("symplektischerEulerA.jl")
 
 function testAdvectionTri()
-  filename = "testV"
+  filename = "testBubbleY"
 
   #order: comp, compHigh, compRec, compDG
   femType=Dict(:p=>[:DG0, :P1, :DG1, :DG0], :v=>[:RT0, :VecP1, :VecDG1, :RT0B], :b=>[:DG0, :P1, :DG1, :DG0]);
@@ -14,8 +13,8 @@ function testAdvectionTri()
 
   taskRecovery=true;
 
-  m=generateTriMesh(300,20,:periodic,:periodic,0.0,300000.0,0.0,10000.0); #(east/west, top/bottom)
-  #m=generateTriMesh(100,100,:periodic,:periodic,0.0,20000.0,0.0,20000.0); #(east/west, top/bottom)
+  #m=generateTriMesh(300,20,:periodic,:periodic,0.0,300000.0,0.0,10000.0); #(east/west, top/bottom)
+  m=generateTriMesh(160,80,:periodic,:periodic,-10000.0,10000.0,0.0,10000.0); #(east/west, top/bottom)
   #m=generateTriMesh(3,3,:periodic,:periodic,0.0,6.0,0.0,6.0); #(east/west, top/bottom)
   #adaptGeometry!(m,(0.3,0.3),false); #sin perbutation
   p=femProblem(m, femType, t=:boussinesq, taskRecovery=taskRecovery);
@@ -27,26 +26,28 @@ function testAdvectionTri()
   #MISMethod=MIS(:MIS_Euler);
   MISMethod=MIS(:MIS4_4);
 
-  dt=10.0;
+  #dt=10.0;
+  dt=1.0;
   #dt=0.5;
   ns=19;
   #EndTime=1*dt;
-  EndTime=2000.0;
+  EndTime=1000.0;
   nIter=Int64(EndTime/dt);
 
   #start function
-  xR=m.geometry.r[1]; xL=m.geometry.l[1]; yR=m.geometry.r[2]; yL=m.geometry.l[2]
-  b0=0.01; H=10000; A=5000;
-  xM=0.5*(xL+xR);
-  function fb1(xz::Array{Float64,1})
-        x=xz[1]; z=xz[2];
-        return b0*sin(pi*z/H)/(1+((x-xM)/A)^2);
-  end
-  #fb2(x,y)=1.0
-  #function fb(x,y)
-  #  rad=sqrt((x-xM)^2+(y-zM)^2);
-  #  return 0.0+(rad<1000)*(2.0*cos(0.5*pi*rad/1000)^2);
+  xR=m.geometry.r[1]; xL=m.geometry.l[1]; zR=m.geometry.r[2]; zL=m.geometry.l[2]
+  b0=0.01; H=10000; A=5000; r0=2000.0;
+  xM=0.5*(xL+xR); zM=0.5*(zL+zR);
+  #function fb1(xz::Array{Float64,1})
+  #      x=xz[1]; z=xz[2];
+  #      return b0*sin(pi*z/H)/(1+((x-xM)/A)^2);
   #end
+  #fb2(x,y)=1.0
+  function fb1(xz::Array{Float64,1})
+    x=xz[1]; z=xz[2];
+    rad=sqrt((x-xM)^2+(z-zM)^2);
+    return 0.0+(rad<r0)*(2.0*cos(0.5*pi*rad/r0)^2);
+  end
   function fb2(xz::Array{Float64,1})
     x=xz[1]; z=xz[2];
     return (x>1 && x<2 && z>1 && z<2)*1.0
@@ -58,8 +59,8 @@ function testAdvectionTri()
     return 0
   end
   #fb(x,y)=1.0;
-  f=Dict(:b=>fb1)
-  #f=Dict(:v=>[fb1,fv2])
+  #f=Dict(:b=>fb1)
+  f=Dict(:v=>[fv2,fb1])
 
   assembMass!(p);
   assembStiff!(p);
@@ -84,8 +85,18 @@ function testAdvectionTri()
 
   y=p.solution[0.0];
   Time=0.0;
+
+  gamma=0.5;
+
+  #time-stepping
+  RKadvection!(p,gamma,Vfcomp,Vf,nquadPhi,nquadPoints,f,Time,dt,EndTime);
+
+
+
+#=
   for i=1:nIter
-    y=splitExplicit(p,gamma,Vfcomp,Vf,nquadPhi,nquadPoints,MISMethod,y,Time,dt,ns);
+    #y=splitExplicit(p,gamma,Vfcomp,Vf,nquadPhi,nquadPoints,MISMethod,f,Time,dt);
+    y=RKadvection!(p,gamma,Vfcomp,Vf,nquadPhi,nquadPoints,f,Time,dt);
     Time=Time+dt
     p.solution[Time]=y;
     if mod(i,10)==0
@@ -94,6 +105,7 @@ function testAdvectionTri()
     end
     println(Time)
   end
+=#
 
   #Speichern des Endzeitpunktes als vtu-Datei:
   #unstructured_vtk(p, EndTime, [:p, :b, :v], ["Pressure", "Buoyancy", "Velocity"], "testAdvectionTriangles/"*filename)
