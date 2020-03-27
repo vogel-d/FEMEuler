5c1d2d8a23ad9c1f51efd99ac3c8050cb7b947fd
rug-levin <levinrug98@gmail.com>
2020-03-27 09:07:23 +0100

    now only needs one call to avoid unneccessary allocations and variable creating

--- RKadvection.jl
+++ RKadvection.jl
@@ -1,7 +1,8 @@
 #Lösen der Akkustik-Gleichung mit Buoyancy
 function RKadvection!(p::femProblem, gamma::Float64, Vfcomp::Symbol,
                       Vfval::SparseVector{Float64,Int64}, nquadPhi::Dict{Symbol, Array{Array{Array{Float64,1},2},1}},
-                      nquadPoints::Array{Array{Float64,2},1}, f::solution, Time::Float64, dt::Float64, EndTime::Float64)
+                      nquadPoints::Array{Array{Float64,2},1}, Time::Float64, dt::Float64, EndTime::Float64,
+                      method::Symbol)
 
   timesteps=(EndTime-Time)/dt;
   !isequal(mod(timesteps,1),0) && @warn("Es existiert kein ganzzahliges k, sodass k*dt=$EndTime. Endzeitpunkt dieser Berechnung: $(Time + floor(timesteps)*dt)")
@@ -12,17 +13,18 @@ function RKadvection!(p::femProblem, gamma::Float64, Vfcomp::Symbol,
   Fv=p.massM[p.femType[:v][1]];
   Fb=p.massM[p.femType[:b][1]];
 
-  f=copy(p.solution[Time])
-  #f=createSolution(length(p.solution[Time].v),length(p.solution[Time].p),length(p.solution[Time].b));
-  #f.p=copy(p.solution[time].p);
-  #f.b=copy(p.solution[time].b);
-  #f.v=copy(p.solution[time].v);
+  #f=copy(p.solution[Time])
 
   np=p.degFBoundary[p.femType[:p][1]].num
   nv=p.degFBoundary[p.femType[:v][1]].num
   nb=p.degFBoundary[p.femType[:b][1]].num
 
   if method==:euler
+    f=createSolution(length(p.solution[Time].v),length(p.solution[Time].p),length(p.solution[Time].b));
+    f.p=copy(p.solution[time].p);
+    f.b=copy(p.solution[time].b);
+    f.v=copy(p.solution[time].v);
+
     for i in 1:timesteps
       Sp, Sb, Sv = advection(p,gamma,Vfval,Vfcomp,f,nquadPoints,nquadPhi);
       f.v[1:nv]=f.v[1:nv]+dt*(Fv\Sp);
@@ -33,10 +35,15 @@ function RKadvection!(p::femProblem, gamma::Float64, Vfcomp::Symbol,
       Time+=dt;
     end
   elseif method==:rk4
-    f1=copy(f);
-    f2=copy(f);
-    f3=copy(f);
-    f4=copy(f);
+    f =createSolution(length(p.solution[Time].v),length(p.solution[Time].p),length(p.solution[Time].b));
+    f1=createSolution(length(p.solution[Time].v),length(p.solution[Time].p),length(p.solution[Time].b));
+    f2=createSolution(length(p.solution[Time].v),length(p.solution[Time].p),length(p.solution[Time].b));
+    f3=createSolution(length(p.solution[Time].v),length(p.solution[Time].p),length(p.solution[Time].b));
+    f4=createSolution(length(p.solution[Time].v),length(p.solution[Time].p),length(p.solution[Time].b));
+
+    f.p=copy(p.solution[Time].p);
+    f.b=copy(p.solution[Time].b);
+    f.v=copy(p.solution[Time].v);
 
     for i in 1:timesteps
       Sp, Sb, Sv = advection(p,gamma,Vfval,Vfcomp,f,nquadPoints,nquadPhi);
@@ -59,9 +66,11 @@ function RKadvection!(p::femProblem, gamma::Float64, Vfcomp::Symbol,
       f4.p[1:np]=Fp\Sp
       f4.b[1:nb]=Fb\Sb
 
-      f.v[1:nv]=f.v[1:nv]+dt*(1/6)*(v1[1:nv]+2*v2[1:nv]+2*v3[1:nv]+v4[1:nv]);
-      f.p[1:np]=f.p[1:np]+dt*(1/6)*(p1[1:np]+2*p2[1:np]+2*p3[1:np]+p4[1:np]);
-      f.b[1:nb]=f.b[1:nb]+dt*(1/6)*(b1[1:nb]+2*b2[1:nb]+2*b3[1:nb]+b4[1:nb]);
+      f.v[1:nv]=f.v[1:nv]+dt*(1/6)*(f1.v[1:nv]+2*f2.v[1:nv]+2*f3.v[1:nv]+f4.v[1:nv]);
+      f.p[1:np]=f.p[1:np]+dt*(1/6)*(f1.p[1:np]+2*f2.p[1:np]+2*f3.p[1:np]+f4.p[1:np]);
+      f.b[1:nb]=f.b[1:nb]+dt*(1/6)*(f1.b[1:nb]+2*f2.b[1:nb]+2*f3.b[1:nb]+f4.b[1:nb]);
+
+
 
       p.solution[Time+dt]=copy(f);
       Time+=dt;
