f5d04aa7937505290f87f02f16a3e5558317c609
rug-levin <levinrug98@gmail.com>
2020-04-15 14:46:58 +0200

    modules file to load compound functions

---
+++ modulesBcompound.jl
@@ -0,0 +1,48 @@
+using SparseArrays;
+using LinearAlgebra;
+using SuiteSparse;
+using Plots;
+
+#pyplot()
+gr() #Pkg.build("CodecZlib") #Pkg.build("LightXML")
+
+include("solution.jl");
+@solution(v,p,b);
+include("constants.jl")
+include("getKub.jl");
+include("getQuad.jl");
+include("meshTypes.jl");
+include("meshFunctions.jl");
+include("findall.jl");
+include("getSubCells.jl")
+include("getSpace.jl")
+include("transformation.jl")
+
+include("getOrderBoundary.jl")
+include("getElementProperties.jl")
+include("degF.jl");
+include("generateMesh.jl")
+include("refineMesh.jl")
+include("compoundData.jl")
+include("femProblem.jl");
+
+include("l2g.jl")
+include("jacobiCompound.jl");
+include("initJacobi.jl")
+include("adaptGeometry.jl")
+include("additionalFunctions.jl")
+
+include("assembMass.jl");
+include("assembMassCompound.jl");
+include("assembLoad.jl");
+include("assembStiff.jl");
+include("applyStartValues.jl");
+include("solveB.jl");
+
+include("plotSolution.jl");
+include("plotSolutionGif.jl");
+include("plotMesh.jl");
+include("plotFEM.jl")
+include("vtk.jl");
+include("vtkTest.jl");
+include("jld.jl");
