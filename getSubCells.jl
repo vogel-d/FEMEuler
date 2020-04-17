78bc1a5265635c4a4e6f0365d4c821aa6971d6a1
rug-levin <levinrug98@gmail.com>
2020-04-17 09:20:55 +0200

    added HexToTris and rewrote HexToKites

--- getSubCells.jl
+++ getSubCells.jl
@@ -1,21 +1,70 @@
-function getSubCellsHexToKites!(subcoord,c::Array{Float64,2})
-    #beginning from lower right kite
-    middle=(1/6).*sum(c,dims=2);
-    subcoord[1]=[0.5*(c[1,2]+c[1,3]) c[1,3] 0.5*(c[1,3]+c[1,4]) middle[1];
-                 0.5*(c[2,2]+c[2,3]) c[2,3] 0.5*(c[2,3]+c[2,4]) middle[2]];
+function getSubCellsHexToKites(m::mesh)
+    subCells=Array{Array{Array{Float64,2},1},1}();
+    subCellsCell=[[0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  ]
+    for Cell in 1:m.topology.size[m.geometry.dim+1]
+        c=m.geometry.coordinates[:,m.topology.incidence["20"][((Cell-1)*6+1):(Cell*6)]];
+        middle=(1/6).*sum(c,dims=2);
+        for subCell in 1:6
+            subCellsCell[subCell]=[0.5*(c[1,mod(subCell,6)+1]+c[1,mod(1+subCell,6)+1]) c[1,mod(1+subCell,6)+1] 0.5*(c[1,mod(1+subCell,6)+1]+c[1,mod(2+subCell,6)+1]) middle[1];
+                                   0.5*(c[2,mod(subCell,6)+1]+c[2,mod(1+subCell,6)+1]) c[2,mod(1+subCell,6)+1] 0.5*(c[2,mod(1+subCell,6)+1]+c[2,mod(2+subCell,6)+1]) middle[2]]
+        end
+        push!(subCells,subCellsCell);
+    end
+    return subCells;
+end
 
-    subcoord[2]=[0.5*(c[1,3]+c[1,4]) c[1,4] 0.5*(c[1,4]+c[1,5]) middle[1];
-                 0.5*(c[2,3]+c[2,4]) c[2,4] 0.5*(c[2,4]+c[2,5]) middle[2]];
 
-    subcoord[3]=[0.5*(c[1,4]+c[1,5]) c[1,5] 0.5*(c[1,5]+c[1,6]) middle[1];
-                 0.5*(c[2,4]+c[2,5]) c[2,5] 0.5*(c[2,5]+c[2,6]) middle[2]];
+function getSubCellsHexToTris(m::mesh)
+    subCells=Array{Array{Array{Float64,2},1},1}();
+    subCellsCell=[[0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  [0.0 0.0 0.0 0.0;
+                   0.0 0.0 0.0 0.0],
+                  ]
+    for Cell in 1:m.topology.size[m.geometry.dim+1]
+        c=m.geometry.coordinates[:,m.topology.incidence["20"][((Cell-1)*6+1):(Cell*6)]];
+        middle=(1/6).*sum(c,dims=2);
+        for subCell in 1:2:11
+            subCellsCell[subCell]=[0.5*(c[1,mod(subCell,6)+1]+c[1,mod(1+subCell,6)+1]) c[1,mod(1+subCell,6)+1] middle[1];
+                                   0.5*(c[2,mod(subCell,6)+1]+c[2,mod(1+subCell,6)+1]) c[2,mod(1+subCell,6)+1] middle[2]]
 
-    subcoord[4]=[0.5*(c[1,5]+c[1,6]) c[1,6] 0.5*(c[1,6]+c[1,1]) middle[1];
-                 0.5*(c[2,5]+c[2,6]) c[2,6] 0.5*(c[2,6]+c[2,1]) middle[2]];
+            subCellsCell[subCell+1]=[c[1,mod(1+subCell,6)+1] 0.5*(c[1,mod(1+subCell,6)+1]+c[1,mod(2+subCell,6)+1]) middle[1];
+                                     c[2,mod(1+subCell,6)+1] 0.5*(c[2,mod(1+subCell,6)+1]+c[2,mod(2+subCell,6)+1]) middle[2]]
 
-    subcoord[5]=[0.5*(c[1,6]+c[1,1]) c[1,1] 0.5*(c[1,1]+c[1,2]) middle[1];
-                 0.5*(c[2,6]+c[2,1]) c[2,1] 0.5*(c[2,1]+c[2,2]) middle[2]];
-
-    subcoord[6]=[0.5*(c[1,1]+c[1,2]) c[1,2] 0.5*(c[1,2]+c[1,3]) middle[1];
-                 0.5*(c[2,1]+c[2,2]) c[2,2] 0.5*(c[2,2]+c[2,3]) middle[2]];
+        end
+        push!(subCells,subCellsCell);
+    end
+    return subCells;
 end
