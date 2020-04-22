d3c4a914fa3a5067b2fb1fce0ec1b3df2bc035a7
rug-levin <levinrug98@gmail.com>
2020-04-22 15:15:23 +0200

    renamed and deleted minor things

--- assembMassCompound.jl
+++ assembMassCompound.jl
@@ -14,8 +14,6 @@ function assembMassCompound!(p::femProblem)
     comp=Set{Symbol}(s);
     for i in comp
         @time massM=assembMassCompound(p.degFBoundary[i],p.mesh,p.kubPoints,p.kubWeights,p.compoundData);
-        i==:DG0 && global MASSDG=massM
-        i==:RT0 && global MASSRT=massM
         n=p.degFBoundary[i].num
         p.massM[i]=lu(massM[1:n,1:n]);
         p.massMBoundary[i]=lu(massM);
@@ -29,8 +27,8 @@ function assembMassCompound(degF::degF{1,:H1}, m::mesh, kubPoints::Array{Float64
     cols=Int64[];
     vals=Float64[];
     phiRef=degF.phi;
-    iter=length(phiRef);
-    sk=size(kubWeights)
+    nPhiSubElement=length(phiRef);
+    sk=size(kubWeights);
     J=initJacobi((m.geometry.dim,m.topology.dim),sk);
     dJ=Array{Float64,2}(undef,sk);
     coord=Array{Float64,2}(undef,m.geometry.dim,diff(m.topology.offset["20"][1:2])[1]);
@@ -38,35 +36,25 @@ function assembMassCompound(degF::degF{1,:H1}, m::mesh, kubPoints::Array{Float64
     mt=m.meshType;
     nSubCells=compoundData.nSubCells;
     assembledPhi=compoundData.assembledPhi[degF.femType];
+    nPhiCompoundElement=length(assembledPhi);
     subcoord=Array{Array{Float64,2},1}(undef,nSubCells);
 
     for k in 1:m.topology.size[m.topology.dim+1]
-        #get coordinates of compound Cell
-        rstart=m.topology.offset[key][k];
-        rend=m.topology.offset[key][k+1]-1;
-        z=1;
-        for j in rstart:rend
-            for i in 1:2
-                coord[i,z]=m.geometry.coordinates[i,m.topology.incidence[key][j]];
-            end
-            z+=1;
-        end
-
         subcoord=compoundData.getSubCells[k];
         gvertices=l2g(degF,k);
-        for j in 1:iter
-            for i in 1:iter
+        for j in 1:nPhiCompoundElement
+            for i in 1:nPhiCompoundElement
                 currentval=0.0;
                 for subCell in 1:nSubCells
                     jacobi!(J,dJ,kubPoints,subcoord[subCell],mt);
-                    for subj in 1:iter
+                    for subj in 1:nPhiSubElement
                         if assembledPhi[j][subj,subCell]!=0
-                            for subi in 1:iter
+                            for subi in 1:nPhiSubElement
                                 if assembledPhi[i][subi,subCell]!=0
                                     for r in 1:sk[2]
                                         for l in 1:sk[1]
                                             currentval+=assembledPhi[i][subi,subCell]*assembledPhi[j][subj,subCell]*
-                                                        kubWeights[l,r]*phiRef[i][l,r]*phiRef[j][l,r]*abs(dJ[l,r]);
+                                                        kubWeights[l,r]*phiRef[subi][l,r]*phiRef[subj][l,r]*abs(dJ[l,r]);
                                         end
                                     end
                                 end
@@ -92,41 +80,30 @@ function assembMassCompound(degF::degF{2,:H1div}, m::mesh, kubPoints::Array{Floa
     cols=Int64[];
     vals=Float64[];
     phiRef=degF.phi;
-    iter=size(phiRef,2);
+    nPhiSubElement=size(phiRef,2);
     sk=size(kubWeights)
     J=initJacobi((m.geometry.dim,m.topology.dim),sk);
     ddJ=Array{Float64,2}(undef,sk);
-    jphiRef=initJacobi((m.geometry.dim,iter),sk);
+    jphiRef=initJacobi((m.geometry.dim,nPhiSubElement),sk);
     coord=Array{Float64,2}(undef,m.geometry.dim,diff(m.topology.offset["20"][1:2])[1]);
     key="20";
     mt=m.meshType;
     nSubCells=compoundData.nSubCells;
     assembledPhi=compoundData.assembledPhi[degF.femType];
+    nPhiCompoundElement=length(assembledPhi);
     subcoord=Array{Array{Float64,2},1}(undef,nSubCells);
 
     for k in 1:m.topology.size[m.topology.dim+1]
-        #get coordinates of subCells
-        rstart=m.topology.offset[key][k];
-        rend=m.topology.offset[key][k+1]-1;
-        z=1;
-        for j in rstart:rend
-            for i in 1:2
-                coord[i,z]=m.geometry.coordinates[i,m.topology.incidence[key][j]];
-            end
-            z+=1;
-        end
-
         subcoord=compoundData.getSubCells[k];
-
         gvertices=l2g(degF,k);
-        for j in 1:iter
-            for i in 1:iter
+        for j in 1:nPhiCompoundElement
+            for i in 1:nPhiCompoundElement
                 currentval=0.0;
                 for subCell in 1:nSubCells
                     jacobi!(J,ddJ,jphiRef,kubPoints,phiRef,subcoord[subCell],mt);
-                    for subj in 1:iter
+                    for subj in 1:nPhiSubElement
                         if assembledPhi[j][subj,subCell]!=0
-                            for subi in 1:iter
+                            for subi in 1:nPhiSubElement
                                 if assembledPhi[i][subi,subCell]!=0
                                     for r in 1:sk[2]
                                         for l in 1:sk[1]
