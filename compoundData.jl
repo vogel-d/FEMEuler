1033849323ac0f4841cf6ba8b15957b36c244fb9
rug-levin <levinrug98@gmail.com>
2020-04-22 15:15:47 +0200

    added method field

--- compoundData.jl
+++ compoundData.jl
@@ -1,17 +1,18 @@
 struct compoundData
+    method::Symbol;
     nSubCells::Int64;
     getSubCells::Array{Array{Array{Float64,2},1},1};
     assembledPhi::Dict{Symbol,Array{Array{Float64,2},1}};
 end
 
 function createCompoundData()
-    compoundData(0,:zero,Dict());
+    compoundData(:nomethod,0,[[zeros(1,1)]],Dict());
 end
 
 function createCompoundData(method::Symbol,femElements::Set{Symbol},m::mesh)
     if method==:HexToKites
-        return compoundData(6,getSubCellsHexToKites(m),assemblePhiHexToKites(femElements))
+        return compoundData(:HexToKites,6,getSubCellsHexToKites(m),assemblePhiHexToKites(femElements))
     elseif method==:HexToTris
-        return compoundData(12,getSubCellsHexToTris(m),assemblePhiHexToTris(femElements))
+        return compoundData(:HexToTris,12,getSubCellsHexToTris(m),assemblePhiHexToTris(femElements))
     end
 end
