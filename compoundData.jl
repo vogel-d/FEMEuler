a5a376e68602a091261a925564758a4aae17a917
rug-levin <levinrug98@gmail.com>
2020-04-26 11:53:48 +0200

    added method parameter

--- compoundData.jl
+++ compoundData.jl
@@ -1,6 +1,6 @@
-struct compoundData
-    method::Symbol;
+struct compoundData{method}
     nSubCells::Int64;
+    nCompoundPhi::Dict{Symbol,Int64};
     getSubCells::Array{Array{Array{Float64,2},1},1};
     assembledPhi::Dict{Symbol,Array{Array{Float64,2},1}};
 end
@@ -11,8 +11,8 @@ end
 
 function createCompoundData(method::Symbol,femElements::Set{Symbol},m::mesh)
     if method==:HexToKites
-        return compoundData(:HexToKites,6,getSubCellsHexToKites(m),assemblePhiHexToKites(femElements))
+        return compoundData{:HexToKites}(6,Dict(:RT0=>6, :DG0=>1),getSubCellsHexToKites(m),assemblePhiHexToKites(femElements))
     elseif method==:HexToTris
-        return compoundData(:HexToTris,12,getSubCellsHexToTris(m),assemblePhiHexToTris(femElements))
+        return compoundData{:HexToTris}(12,Dict(:RT0=>6, :DG0=>1),getSubCellsHexToTris(m),assemblePhiHexToTris(femElements))
     end
 end
