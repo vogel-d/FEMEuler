41cfc8e1ca0a4e9b536d82cd840474e34eb0942b
rug-levin <levinrug98@gmail.com>
2020-04-26 14:48:28 +0200

    reordered sub cells for simplicity

--- getSubCells.jl
+++ getSubCells.jl
@@ -2,19 +2,19 @@ function getSubCells!(subcoord::Array{Array{Float64,2},1}, coord::Array{Float64,
     middle=(1/6).*sum(coord,dims=2); #allocation
 
     for i in 1:6
-        subcoord[(i-1)*2+1]= [coord[1,1+mod(i+1,6)] 0.5*(coord[1,1+mod(i+1,6)]+coord[1,1+mod(i+2,6)]) middle[1];
-                              coord[2,1+mod(i+1,6)] 0.5*(coord[2,1+mod(i+1,6)]+coord[2,1+mod(i+2,6)]) middle[2]];
+        subcoord[(i-1)*2+1]= [coord[1,i] 0.5*(coord[1,i]+coord[1,1+mod(i,6)]) middle[1];
+                              coord[2,i] 0.5*(coord[2,i]+coord[2,1+mod(i,6)]) middle[2]];
 
-        subcoord[i*2]=       [0.5*(coord[1,1+mod(i+1,6)]+coord[1,1+mod(i+2,6)]) coord[1,1+mod(i+2,6)] middle[1];
-                              0.5*(coord[2,1+mod(i+1,6)]+coord[2,1+mod(i+2,6)]) coord[2,1+mod(i+2,6)] middle[2]];
+        subcoord[i*2]=       [0.5*(coord[1,i]+coord[1,1+mod(i,6)]) coord[1,1+mod(i,6)] middle[1];
+                              0.5*(coord[2,i]+coord[2,1+mod(i,6)]) coord[2,1+mod(i,6)] middle[2]];
     end
 end
 
 
 function getSubCells!(subcoord::Array{Array{Float64,2},1}, coord::Array{Float64,2}, compoundData::compoundData{:HexToKites})
     middle=(1/6).*sum(coord,dims=2);
-    for subCell in 1:6
-        subcoord[subCell]=[0.5*(coord[1,mod(subCell,6)+1]+coord[1,mod(1+subCell,6)+1]) coord[1,mod(1+subCell,6)+1] 0.5*(coord[1,mod(1+subCell,6)+1]+coord[1,mod(2+subCell,6)+1]) middle[1];
-                           0.5*(coord[2,mod(subCell,6)+1]+coord[2,mod(1+subCell,6)+1]) coord[2,mod(1+subCell,6)+1] 0.5*(coord[2,mod(1+subCell,6)+1]+coord[2,mod(2+subCell,6)+1]) middle[2]]
+    for i in 1:6
+        subcoord[i]=[0.5*(coord[1,mod(i+4,6)+1]+coord[1,i]) coord[1,i] 0.5*(coord[1,i]+coord[1,mod(i,6)+1]) middle[1];
+                     0.5*(coord[2,mod(i+4,6)+1]+coord[2,i]) coord[2,i] 0.5*(coord[2,i]+coord[2,mod(i,6)+1]) middle[2]]
     end
 end
