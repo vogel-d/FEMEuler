1181ec9c7fb7808cb0905b02465aae1ec5be2a6f
rug-levin <levinrug98@gmail.com>
2020-04-27 13:02:38 +0200

    applying third constraint via edge-integrating and added assemblePhi routine for kites

--- assembleCompoundPhi.jl
+++ assembleCompoundPhi.jl
@@ -7,13 +7,10 @@ function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{A
 end
 
 
-function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{Array{Float64,2},1}, degF::degF{2,:H1div}, m::mesh, J, ddJ, jphi, kubPoints, kubWeights, compoundData::compoundData{:HexToTris})
-    phi=degF.phi;
-    divphi=degF.divphi;
+function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{Array{Float64,2},1}, m::mesh, divphi, J, ddJ, jphi, nquadPhi, nquadPoints, quadWeights, compoundData::compoundData{:HexToTris})
     mt=m.meshType;
-    sk=size(kubWeights);
 
-    nPhiSubCell=size(phi,2);
+    nPhiSubCell=size(jphi,2);
     nSubCells=compoundData.nSubCells;
     A=zeros(nPhiSubCell*nSubCells,nPhiSubCell*nSubCells);
     b=zeros(nPhiSubCell*nSubCells);
@@ -46,20 +43,18 @@ function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{A
             tangent[i]=subcoord[subCell][i,2]-subcoord[subCell][i,1];
         end
         @. tangent=tangent/sqrt(tangent[1]^2+tangent[2]^2);
-        jacobi!(J,ddJ,jphi,kubPoints,phi,subcoord[subCell],mt);
+        jacobi!(J,ddJ,jphi,nquadPoints[1],nquadPhi[1],subcoord[subCell],mt);
         for i in 1:nPhiSubCell
-            for r in 1:sk[2]
-                for l in 1:sk[1]
-                    for d in 1:m.geometry.dim
-                        dotp+=jphi[d,i][l,r]*tangent[d];
-                    end
-                    #apply third constraint
-                    A[end, (i-1)*12+subCell]+=kubWeights[l,r]*(ddJ[l,r]/abs(ddJ[l,r]))*dotp;
-                    #saving ddJ for divergence-constraint
-                    #TODO: check where to compute divergence for kites since its non-constant
-                    divCoeff[subCell]=copy(ddJ[1]);
+            for r in 1:length(quadWeights)
+                dotp=0.0;
+                for d in 1:m.geometry.dim
+                    dotp+=jphi[d,i][r]*tangent[d];
                 end
+                #apply third constraint
+                A[end, (i-1)*12+subCell]+=quadWeights[r]*(ddJ[r]/abs(ddJ[r]))*dotp;
             end
+            #saving ddJ for divergence-constraint
+            divCoeff[subCell]=copy(ddJ[1]);
         end
     end
 
@@ -79,23 +74,7 @@ function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{A
     end
 
     betas= A\b;
-#=
-    betas2times=Array{Float64,2}(undef,nPhiSubCell,24);
-    betas2times[1,1:12]  = betas[1:12];
-    betas2times[1,13:24] = betas[1:12];
-    betas2times[2,1:12]  = betas[13:24];
-    betas2times[2,13:24] = betas[13:24];
-    betas2times[3,1:12]  = betas[25:36];
-    betas2times[3,13:24] = betas[25:36];
 
-    assembledPhi[1] = betas2times[:,13:24];
-    assembledPhi[2] = betas2times[:,11:22];
-    assembledPhi[3] = betas2times[:,9:20];
-    #minus needed for correct normals
-    assembledPhi[4] = -betas2times[:,7:18];
-    assembledPhi[5] = -betas2times[:,5:16];
-    assembledPhi[6] = -betas2times[:,3:14];
-    =#
     for compoundPhi in 1:3
         for subPhi in 1:3
             for subElement in 1:12
@@ -115,70 +94,95 @@ function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{A
 end
 
 
-function assemblePhi!(CellID::Int64, degF::degF{2,:H1div}, dJ, compoundData::compoundData{:HexToKites})
-    phi=degF.phi;
-    divphi=degF.divphi;
-    nPhiSubCell=size(phi,2);
+
+function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{Array{Float64,2},1}, m::mesh, divphi, J, ddJ, jphi, nquadPhi, nquadPoints, quadWeights, compoundData::compoundData{:HexToKites})
+    mt=m.meshType;
+
+    nPhiSubCell=size(jphi,2);
     nSubCells=compoundData.nSubCells;
     A=zeros(nPhiSubCell*nSubCells,nPhiSubCell*nSubCells);
     b=zeros(nPhiSubCell*nSubCells);
     tangent=zeros(2);
     divCoeff=zeros(nSubCells);
-    subcoord=compoundData.getSubCells[CellID];
-
 
     #first constraint
     #outer edges
-    for i in 1:nSubCells
+    for i in 1:12
         A[i,i]=1.0;
         b[i]=0.0;
     end
-    b[1]=1.0
-    b[2]=1.0
+    b[7]=1.0;
+    b[2]=1.0;
 
     #inner edges
     #last two subCell-ansatzfunctions correspond to inner edges
     #so second to last beta from one subCell should match last beta from next subCell
-    for i in 1:nSubCells
+    for i in 1:6
         #add second to last beta from current subCell
-        A[nSubCells+i,(nPhiSubCell-2)*nSubCells+i]=1.0;
+        A[12+i,12+i]=1.0;
         #add last from next subCell
-        A[nSubCells+i,(nPhiSubCell-1)*nSubCells+mod(i,nSubCells)+1]=-1.0;
-    #    A[12+i,24+mod(i,12)+1]=1.0;
-        b[nSubCells+i]=0.0;
+        #A[12+i,18+mod(i,6)+1]=-1.0;
+        A[12+i,18+mod(i,6)+1]=1.0;
     end
 
-
-    for subCell in 1:nSubCells
-        tangent=(subcoord[subCell][:,2].-subcoord[subCell][:,1]);
-        tangent=tangent./norm(tangent,2);
-        jacobi!(J,ddJ,jphi,kubPoints,phi,subcoord[subCell],mt);
-        for i in 1:nPhiSubCell
-            for r in 1:sk[2]
-                for l in 1:sk[1]
+    dotp=0.0;
+    for subCell in 1:6
+        for edge in 1:2
+            for i in 1:2
+                tangent[i]=subcoord[subCell][i,edge+1]-subcoord[subCell][i,edge];
+            end
+            @. tangent=tangent/sqrt(tangent[1]^2+tangent[2]^2);
+            jacobi!(J,ddJ,jphi,nquadPoints[edge],nquadPhi[edge],subcoord[subCell],mt);
+            for i in 1:nPhiSubCell
+                for r in 1:length(quadWeights)
                     dotp=0.0;
                     for d in 1:m.geometry.dim
-                        dotp+=jphi[d,i][l,r]*tangent[d];
+                        dotp+=jphi[d,i][r]*tangent[d];
                     end
-                    #applying third constraint
-                    A[end, (i-1)*nSubCells+subCell]+=kubWeights[l,r]*(ddJ[l,r]/abs(ddJ[l,r]))*dotp;
+                    #apply third constraint
+                    A[end, (i-1)*6+subCell]+=quadWeights[r]*(ddJ[r]/abs(ddJ[r]))*dotp;
                     #saving ddJ for divergence-constraint
-                    #TODO: check where to compute divergence for kites since its non-constant
-                    divCoeff[subCell]=ddJ[1];
+                    divCoeff[subCell]=copy(ddJ[1]);
                 end
             end
         end
     end
 
+    #TODO: check where to compute divergence since its non-constant
+    divCoeff=ones(nSubCells);
     #apply second constraint (divergence)
-    for i in 1:11
+    for i in 1:5
         #FEMEuler divergences
-        A[+i,[1,12+1,24+1]]=[1.0,1.0,-1.0];
-        A[24+i,[1+i,12+1+i,24+1+i]]=[-1.0,-1.0,1.0];
+        A[18+i,1]=      1*divCoeff[1];
+        A[18+i,6+1]=    1*divCoeff[1];
+        A[18+i,12+1]=   (-1)*divCoeff[1];
+        A[18+i,18+1]=   (-1)*divCoeff[1];
+
+        A[18+i,1+i]=    (-1)*divCoeff[i+1];
+        A[18+i,6+1+i]=  (-1)*divCoeff[i+1];
+        A[18+i,12+1+i]= 1*divCoeff[i+1];
+        A[18+i,18+1+i]= 1*divCoeff[i+1];
         #MelvinThuburn divergences (common divergences)
     #    A[24+i,[1,12+1,24+1]]=[1.0,1.0,1.0];
     #    A[24+i,[1+i,12+1+i,24+1+i]]=[-1.0,-1.0,-1.0];
-        b[24+i]=0.0;
     end
 
+    betas= A\b;
+
+    for compoundPhi in 1:3
+        for subPhi in 1:4
+            for subElement in 1:6
+                #                                                                   walking through 1:6 cyclic; starting from 1, then 6, then 5, ...
+                assembledPhi[compoundPhi][subPhi,subElement] = betas[(subPhi-1)*6+(1+mod(-1+subElement-(compoundPhi-1),6))]
+            end
+        end
+    end
+    for compoundPhi in 4:6
+        for subPhi in 1:4
+            for subElement in 1:6
+                #                                                                   walking through 1:6 cyclic; starting from 1, then 6, then 5, ...
+                assembledPhi[compoundPhi][subPhi,subElement] = -betas[(subPhi-1)*6+(1+mod(-1+subElement-(compoundPhi-1),6))]
+            end
+        end
+    end
 end
