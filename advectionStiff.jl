6a2fe2e39911863768b78f99f49833d08aa007b3
rug-levin <levinrug98@gmail.com>
2020-04-30 11:31:53 +0200

    added compoundData argument

--- advectionStiff.jl
+++ advectionStiff.jl
@@ -1,8 +1,9 @@
 function advectionStiff(degFT::degF{1,:H1}, phiTtrans::Array{Array{Array{Float64,1},2},1},
                         degFF::degF{2,:H1div}, phiFtrans::Array{Array{Array{Float64,1},2},1}, fval::SparseVector{Float64,Int64},
                         degFW::degF{1,:H1}, phiWtrans::Array{Array{Array{Float64,1},2},1}, wval::Array{Float64,1},
-                        gamma::Float64,m::mesh, kubPoints::Array{Float64,2}, kubWeights::Array{Float64,2},
-                        nquadPoints::Array{Array{Float64,2},1}, edgeData::Array{Array{Int64,1},1})
+                        gamma::Float64, m::mesh, kubPoints::Array{Float64,2}, kubWeights::Array{Float64,2},
+                        nquadPoints::Array{Array{Float64,2},1}, edgeData::Array{Array{Int64,1},1},
+                        compoundData::compoundData)
 
     phiT=@views degFT.phi;
     phiF=@views degFF.phi;
@@ -45,7 +46,8 @@ function advectionStiff(degFT::degF{2,:H1div}, phiTtrans::Array{Array{Array{Floa
                         degFF::degF{2,:H1div}, phiFtrans::Array{Array{Array{Float64,1},2},1},  fval::SparseVector{Float64,Int64},
                         degFW::degF{2,S} where S, phiWtrans::Array{Array{Array{Float64,1},2},1}, wval::Array{Float64,1},
                         gamma::Float64,m::mesh, kubPoints::Array{Float64,2}, kubWeights::Array{Float64,2},
-                        nquadPoints::Array{Array{Float64,2},1}, edgeData::Array{Array{Int64,1},1})
+                        nquadPoints::Array{Array{Float64,2},1}, edgeData::Array{Array{Int64,1},1},
+                        compoundData::compoundData)
 
     phiT=@views degFT.phi;
     phiF=@views degFF.phi;
