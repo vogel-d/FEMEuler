fb6b027b3cd2a4df0ca79903151b56c25dacb173
rug-levin <levinrug98@gmail.com>
2020-05-14 10:10:23 +0200

    added RectToKite option (removed allocations)

--- assembleCompoundPhi.jl
+++ assembleCompoundPhi.jl
@@ -6,6 +6,10 @@ function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{A
     fill!(assembledPhi,ones(Float64,1,6));
 end
 
+function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{Array{Float64,2},1}, degF::degF{1,:H1}, m::mesh, J, dJ, phi, kubPoints, kubWeights, compoundData::compoundData{:RectToKites})
+    fill!(assembledPhi,ones(Float64,1,4));
+end
+
 
 function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{Array{Float64,2},1}, m::mesh, divphi, J, ddJ, jphi, nquadPhi, nquadPoints, quadWeights, compoundData::compoundData{:HexToTris})
     mt=m.meshType;
@@ -100,10 +104,12 @@ function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{A
 
     nPhiSubCell=size(jphi,2);
     nSubCells=compoundData.nSubCells;
-    A=zeros(nPhiSubCell*nSubCells,nPhiSubCell*nSubCells);
-    b=zeros(nPhiSubCell*nSubCells);
-    tangent=zeros(2);
-    divCoeff=zeros(nSubCells);
+    A=Array{Float64,2}(undef,nPhiSubCell*nSubCells,nPhiSubCell*nSubCells);
+    fill!(A,0.0)
+    b=Array{Float64,1}(undef,nPhiSubCell*nSubCells);
+    fill!(b,0.0)
+    tangent=Array{Float64,1}(undef,2);
+    divCoeff=Array{Float64,1}(undef,nSubCells);
 
     #first constraint
     #outer edges
@@ -143,6 +149,9 @@ function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{A
                     A[end, (i-1)*6+subCell]+=quadWeights[r]*(ddJ[r]/abs(ddJ[r]))*dotp;
                     #saving ddJ for divergence-constraint
                     divCoeff[subCell]=copy(ddJ[1]);
+                    if divCoeff[subCell]<0.0
+                        println("negative determinant while assembling phi. not considered");
+                    end
                 end
             end
         end
@@ -185,4 +194,138 @@ function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{A
             end
         end
     end
+end
+
+
+function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{Array{Float64,2},1}, m::mesh, divphi, J, ddJ, jphi, nquadPhi, nquadPoints, quadWeights, compoundData::compoundData{:RectToKites})
+    mt=m.meshType;
+
+    nPhiSubCell=size(jphi,2);
+    nSubCells=compoundData.nSubCells;
+    A=Array{Float64,2}(undef,nPhiSubCell*nSubCells,nPhiSubCell*nSubCells);
+    fill!(A,0.0)
+    b=Array{Float64,1}(undef,nPhiSubCell*nSubCells);
+    fill!(b,0.0)
+    tangent=Array{Float64,1}(undef,2);
+    divCoeff=Array{Float64,1}(undef,nSubCells);
+
+    #first constraint
+    #outer edges
+    for i in 1:8
+        A[i,i]=1.0;
+    end
+    b[5]=1.0;
+    b[2]=1.0;
+
+    #inner edges
+    #last two subCell-ansatzfunctions correspond to inner edges
+    #so second to last beta from one subCell should match last beta from next subCell
+    for i in 1:4
+        #add second to last beta from current subCell
+        A[8+i,8+i]=1.0;
+        #add last from next subCell
+        A[8+i,12+mod(i,4)+1]=1.0;
+    end
+
+    dotp=0.0;
+    for subCell in 1:4
+        for edge in 1:2
+            for i in 1:2
+                tangent[i]=subcoord[subCell][i,edge+1]-subcoord[subCell][i,edge];
+            end
+            @. tangent=tangent/sqrt(tangent[1]^2+tangent[2]^2);
+            jacobi!(J,ddJ,jphi,nquadPoints[edge],nquadPhi[edge],subcoord[subCell],mt);
+            for i in 1:nPhiSubCell
+                for r in 1:length(quadWeights)
+                    dotp=0.0;
+                    for d in 1:m.geometry.dim
+                        dotp+=jphi[d,i][r]*tangent[d];
+                    end
+                    #apply third constraint
+                    A[end, (i-1)*4+subCell]+=quadWeights[r]*(ddJ[r]/abs(ddJ[r]))*dotp;
+                    #saving ddJ for divergence-constraint
+                    divCoeff[subCell]=copy(ddJ[1]);
+                    if divCoeff[subCell]<0.0
+                        println("negative determinant while assembling phi. not considered");
+                    end
+                end
+            end
+        end
+    end
+
+    #TODO: check where to compute divergence since its non-constant
+    divCoeff=ones(nSubCells);
+    #apply second constraint (divergence)
+    for i in 1:3
+        #FEMEuler divergences
+        A[12+i,1]=      1*divCoeff[1];
+        A[12+i,4+1]=    1*divCoeff[1];
+        A[12+i,8+1]=    (-1)*divCoeff[1];
+        A[12+i,12+1]=   (-1)*divCoeff[1];
+
+        A[12+i,1+i]=    (-1)*divCoeff[i+1];
+        A[12+i,4+1+i]=  (-1)*divCoeff[i+1];
+        A[12+i,8+1+i]=  1*divCoeff[i+1];
+        A[12+i,12+1+i]= 1*divCoeff[i+1];
+        #MelvinThuburn divergences (common divergences)
+    #    A[24+i,[1,12+1,24+1]]=[1.0,1.0,1.0];
+    #    A[24+i,[1+i,12+1+i,24+1+i]]=[-1.0,-1.0,-1.0];
+    end
+
+    betas= A\b;
+
+    for compoundPhi in 1:2
+        for subPhi in 1:nPhiSubCell
+            for subElement in 1:nSubCells
+                #                                                                   walking through 1:4 cyclic; starting from 1, then 4, then 3, ...
+                assembledPhi[compoundPhi][subPhi,subElement] = betas[(subPhi-1)*4+(1+mod(-1+subElement-(compoundPhi-1),4))]
+            end
+        end
+    end
+    for compoundPhi in 3:4
+        for subPhi in 1:nPhiSubCell
+            for subElement in 1:nSubCells
+                #                                                                   walking through 1:4 cyclic; starting from 1, then 4, then 3, ...
+                assembledPhi[compoundPhi][subPhi,subElement] = -betas[(subPhi-1)*4+(1+mod(-1+subElement-(compoundPhi-1),4))]
+            end
+        end
+    end
+end
+
+
+
+
+
+function assemblePhiHexToKites(femElements::Set{Symbol})
+    assembledPhi=Dict();
+    for type in femElements
+        if type==:RT0
+            #phi2times includes the factors one compound ansatzfunction needs
+            #from each subelement (coloumns) with its ansatzfunctions (rows)
+            #assemblation of every individual phi shows cyclic behaviour
+            phi2times = (1/3)*
+                        [0 3  0  0  0  0 0 3  0  0  0  0;
+                         3 0  0  0  0  0 3 0  0  0  0  0;
+                         0 2  1  0 -1 -2 0 2  1  0 -1 -2;
+                         2 0 -2 -1  0  1 2 0 -2 -1  0  1];
+
+            phi1 = phi2times[:,7:12];
+            phi2 = phi2times[:,6:11];
+            phi3 = phi2times[:,5:10];
+            phi4 = -phi2times[:,4:9];
+            phi5 = -phi2times[:,3:8];
+            phi6 = -phi2times[:,2:7];
+
+            assembledPhi[type]=[phi1, phi2, phi3, phi4, phi5, phi6];
+        elseif type==:DG0
+            phi2times = ones(Float64,1,12);
+
+            phi1 = phi2times[:,7:12];
+
+            assembledPhi[type]=[phi1];
+        else
+            error("Tell me how to assemble HexToKite compound ansatzfunctions in $type")
+        end
+    end
+    return assembledPhi;
 end
