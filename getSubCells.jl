c06d23979dc73fbf7316c0bf7ea02de2840504c9
rug-levin <levinrug98@gmail.com>
2020-05-14 10:02:08 +0200

    removed allocations

--- getSubCells.jl
+++ getSubCells.jl
@@ -11,10 +11,60 @@ function getSubCells!(subcoord::Array{Array{Float64,2},1}, coord::Array{Float64,
 end
 
 
-function getSubCells!(subcoord::Array{Array{Float64,2},1}, coord::Array{Float64,2}, compoundData::compoundData{:HexToKites})
-    middle=(1/6).*sum(coord,dims=2);
+function getSubCells!(subcoord::Array{Array{Float64,2},1}, coord::Array{Float64,2}, center::Array{Float64,1}, compoundData::compoundData{:HexToKites})
+    #middle=(1/6).*sum(coord,dims=2);
+    fill!(center,0.0);
     for i in 1:6
-        subcoord[i]=[0.5*(coord[1,mod(i+4,6)+1]+coord[1,i]) coord[1,i] 0.5*(coord[1,i]+coord[1,mod(i,6)+1]) middle[1];
-                     0.5*(coord[2,mod(i+4,6)+1]+coord[2,i]) coord[2,i] 0.5*(coord[2,i]+coord[2,mod(i,6)+1]) middle[2]]
+        for j in 1:2
+            center[j]+=(1/6)*coord[j,i];
+        end
+    end
+    for i in 1:6
+        for j in 1:2
+            subcoord[i][j,1]=0.5*(coord[j,mod(i+4,6)+1]+coord[j,i])
+        end
+
+        for j in 1:2
+            subcoord[i][j,2]=coord[j,i];
+        end
+
+        for j in 1:2
+            subcoord[i][j,3]=0.5*(coord[j,i]+coord[j,mod(i,6)+1])
+        end
+
+        for j in 1:2
+            subcoord[i][j,4]=center[j];
+        end
+        #subcoord[i]=[0.5*(coord[1,mod(i+4,6)+1]+coord[1,i]) coord[1,i] 0.5*(coord[1,i]+coord[1,mod(i,6)+1]) center[1];
+        #             0.5*(coord[2,mod(i+4,6)+1]+coord[2,i]) coord[2,i] 0.5*(coord[2,i]+coord[2,mod(i,6)+1]) center[2]]
+    end
+end
+
+function getSubCells!(subcoord::Array{Array{Float64,2},1}, coord::Array{Float64,2}, center::Array{Float64,1}, compoundData::compoundData{:RectToKites})
+    #middle=(1/6).*sum(coord,dims=2);
+    fill!(center,0.0);
+    for i in 1:4
+        for j in 1:2
+            center[j]+=(1/4)*coord[j,i];
+        end
+    end
+    for i in 1:4
+        for j in 1:2
+            subcoord[i][j,1]=0.5*(coord[j,mod(i+2,4)+1]+coord[j,i])
+        end
+
+        for j in 1:2
+            subcoord[i][j,2]=coord[j,i];
+        end
+
+        for j in 1:2
+            subcoord[i][j,3]=0.5*(coord[j,i]+coord[j,mod(i,4)+1])
+        end
+
+        for j in 1:2
+            subcoord[i][j,4]=center[j];
+        end
+        #subcoord[i]=[0.5*(coord[1,mod(i+4,6)+1]+coord[1,i]) coord[1,i] 0.5*(coord[1,i]+coord[1,mod(i,6)+1]) center[1];
+        #             0.5*(coord[2,mod(i+4,6)+1]+coord[2,i]) coord[2,i] 0.5*(coord[2,i]+coord[2,mod(i,6)+1]) center[2]]
     end
 end
