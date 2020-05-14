ce5f1439016a804d44e92bf030bf2cd5cc77c119
vogel-d <denise.vogel@gmx.net>
2020-05-14 19:22:57 +0200

    added recovery for vectorial functions (wip)

--- getPhiRecovery.jl
+++ getPhiRecovery.jl
@@ -21,11 +21,7 @@ function getPhiRecovery(m::Array{Float64,1},recoverySpace::Val{:R1S})
     phiz(xyz) = (xyz[3]-m[3]);
 
     phi=[phi1, phix, phiy, phiz]
-    #=
-    phi=[phi1 phix phiy phiz null null null null null null null null;
-         null null null null phi1 phix phiy phiz null null null null;
-         null null null null null null null null phi1 phix phiy phiz]
-    =#
+
     return phi
 end
 
@@ -65,10 +61,80 @@ function getPhiRecovery(m::Array{Float64,1},recoverySpace::Val{:R2S})
     phiz2(xyz) = (xyz[3]-m[3])^2
 
     phi=[phi1, phix, phiy, phiz, phixy, phixz, phiyz, phix2, phiy2, phiz2]
-    #=
+
+    return phi
+end
+
+
+function getPhiRecovery(m::Array{Float64,1},recoverySpace::Val{:VecR1})
+
+    null(xyz) = 0.0
+    phi1(xyz) = 1.0;
+
+    phix(xyz) = (xyz[1]-m[1]);
+    phiy(xyz) = (xyz[2]-m[2]);
+
+    phi=[phi1 phix phiy null null null;
+         null null null phi1 phix phiy]
+
+    return phi
+end
+
+function getPhiRecovery(m::Array{Float64,1},recoverySpace::Val{:VecR1S})
+
+    null(xyz) = 0.0
+    phi1(xyz) = 1.0;
+
+    phix(xyz) = (xyz[1]-m[1]);
+    phiy(xyz) = (xyz[2]-m[2]);
+    phiz(xyz) = (xyz[3]-m[3]);
+
+    phi=[phi1 phix phiy phiz null null null null null null null null;
+         null null null null phi1 phix phiy phiz null null null null;
+         null null null null null null null null phi1 phix phiy phiz]
+
+    return phi
+end
+
+function getPhiRecovery(m::Array{Float64,1},recoverySpace::Val{:VecR2})
+
+    #null(xyz) = 0.0
+    phi1(xyz) = 1.0;
+
+    phix(xyz) = (xyz[1]-m[1]);
+    phiy(xyz) = (xyz[2]-m[2]);
+
+    phixy(xyz) = (xyz[1]-m[1])*(xyz[2]-m[2])
+
+    phix2(xyz) = (xyz[1]-m[1])^2
+    phiy2(xyz) = (xyz[2]-m[2])^2
+
+    phi=[phi1 phix phiy phixy phix2 phiy2 null null null null null null;
+         null null null null null null phi1 phix phiy phixy phix2 phiy2]
+
+    return phi
+end
+
+function getPhiRecovery(m::Array{Float64,1},recoverySpace::Val{:VecR2S})
+
+    #null(xyz) = 0.0
+    phi1(xyz) = 1.0;
+
+    phix(xyz) = (xyz[1]-m[1]);
+    phiy(xyz) = (xyz[2]-m[2]);
+    phiz(xyz) = (xyz[3]-m[3]);
+
+    phixy(xyz) = (xyz[1]-m[1])*(xyz[2]-m[2])
+    phixz(xyz) = (xyz[1]-m[1])*(xyz[3]-m[3])
+    phiyz(xyz) = (xyz[2]-m[2])*(xyz[3]-m[3])
+
+    phix2(xyz) = (xyz[1]-m[1])^2
+    phiy2(xyz) = (xyz[2]-m[2])^2
+    phiz2(xyz) = (xyz[3]-m[3])^2
+
     phi=[phi1 phix phiy phiz phixy phixz phiyz phix2 phiy2 phiz2 null null null null null null null null null null null null null null null null null null null null;
          null null null null null null null null null null phi1 phix phiy phiz phixy phixz phiyz phix2 phiy2 phiz2 null null null null null null null null null null;
          null null null null null null null null null null null null null null null null null null null null phi1 phix phiy phiz phixy phixz phiyz phix2 phiy2 phiz2]
-    =#
+    
     return phi
 end
--- recovery.jl
+++ recovery.jl
@@ -8,13 +8,13 @@ function recovery(degFT::degF{1,:H1}, recoverySpace::Symbol, cval::Array{Float64
                   stencil::Array{Array{Int,1},1}, stencilBoundary::SparseMatrixCSC{Int,Int}, m::mesh, kubPoints::Array{Float64,2}, kubWeights::Array{Float64,2})
     phiT=@views degFT.phi;
     #phiR=@views degFR.phi;
+    phiR=getPhiRecovery([0.0,0.0],Val(recoverySpace));
 
     nf=m.topology.size[3]
     mcoord=m.geometry.coordinates;
     inc=m.topology.incidence["20"];
     off=m.topology.offset["20"];
 
-    phiR=getPhiRecovery([0.0,0.0],Val(recoverySpace));
     nR=length(phiR)
     nT=length(phiT)
     #nR=div(length(phiR),nf)
@@ -30,7 +30,7 @@ function recovery(degFT::degF{1,:H1}, recoverySpace::Symbol, cval::Array{Float64
     globalNumT=Array{Int64,1}(undef,nT);
     lcval=Float64[]
     cR=Float64[];
-    zf=0;
+    #zf=0;
     for f in 1:nf
         #jacobi!(J,dJ,m,f,kubPoints,coord);
         fcoord=@views mcoord[:,inc[off[f]:off[f+1]-1]]
@@ -88,7 +88,98 @@ function recovery(degFT::degF{1,:H1}, recoverySpace::Symbol, cval::Array{Float64
         qrM=qr(lM, Val(true))
         append!(cR,qrM\lcval);
         empty!(lcval);
-        zf+=nR;
+        #zf+=nR;
+    end
+    return cR;
+end
+
+function recovery(degFT::degF{2,:H1div}, recoverySpace::Symbol, cval::Array{Float64,1},
+                  stencil, m::mesh, kubPoints::Array{Float64,2}, kubWeights::Array{Float64,2})
+    phiT=@views degFT.phi;
+    #phiR=@views degFR.phi;
+    phiR=getPhiRecovery([0.0,0.0],Val(recoverySpace));
+
+    nf=m.topology.size[3]
+    mcoord=m.geometry.coordinates;
+    inc=m.topology.incidence["20"];
+    off=m.topology.offset["20"];
+
+    nT=size(phiT,2)
+    nR=size(phiR,2)
+    #nR=div(size(phiR,2),nf)
+    sk=size(kubWeights);
+
+    J=initJacobi((m.geometry.dim,m.topology.dim),sk);
+    dJ=Array{Float64,2}(undef,sk);
+    jphiT=initJacobi((m.geometry.dim,nT),sk);
+    coord=Array{Float64,2}(undef,m.geometry.dim,m.meshType);
+
+    dxy= @. (m.geometry.r-m.geometry.l)/m.topology.n
+
+    globalNumT=Array{Int64,1}(undef,nT);
+    lcval=Float64[]
+    cR=Float64[];
+    #zf=0;
+    for f in 1:nf
+        #jacobi!(J,ddJ,jphiT,m,f,kubPoints,phiT,coord);
+        fcoord=@views mcoord[:,inc[off[f]:off[f+1]-1]]
+        n=transformation(m,fcoord,0.5,0.5);
+        t1,t2=getTangentialPlane(n)
+        #phiR=getPhiRecovery(n,Val(recoverySpace));
+
+        nS=length(stencil[f])
+        lM=zeros(nS*nT,nR)
+        z=1;
+        for k in stencil[f]
+            jacobi!(J,ddJ,jphiT,m,k,kubPoints,phiT,coord);
+            k==f ? w=10.0^8 : w=1.0;
+            b = @. (k==abs(stencilBoundary[:,f]))
+            if iszero(b)
+                kcoord=@views mcoord[:,inc[off[k]:off[k+1]-1]]
+            else
+                kcoord=similar(fcoord);
+                if b[1]==1
+                    if stencilBoundary[1,f]<0
+                        dir=2; inds1=[4,3]; inds2=[1,2]
+                    else
+                        dir=2; inds1=[1,2]; inds2=[4,3]
+                    end
+                else
+                    if stencilBoundary[2,f]<0
+                        dir=1; inds1=[2,3]; inds2=[1,4]
+                    else
+                        dir=1; inds1=[1,4]; inds2=[2,3]
+                    end
+                end
+                kcoord[:,inds1]=fcoord[:,inds2]
+                kcoord[:,inds2]=fcoord[:,inds2]
+                @. kcoord[dir,inds2]=fcoord[dir,inds2]-dxy[dir]
+            end
+
+            for i in 1:nT
+                for j in 1:nR
+                    currentval=0.0;
+                    for r in 1:sk[2]
+                        for l in 1:sk[1]
+                            vecdot=0.0
+                            for d in 1:m.geometry.dim
+                                vecdot+=jphiT[d,i][l,r]*phiR[d,j](transformRecoveryCoord(n,t1,t2,transformation(m,kcoord,kubPoints[1,l],kubPoints[2,r])));
+                            end
+                            currentval+=kubWeights[l,r]*ddJ[l,r]/abs(ddJ[l,r])*vecdot;
+                        end
+                    end
+                    lM[z,j]+=w*currentval;
+                end
+                z+=1;
+            end
+            l2g!(globalNumT,degFT,k)
+            append!(lcval,w.*cval[globalNumT])
+        end
+
+        qrM=qr(lM, Val(true))
+        append!(cR,qrM\lcval);
+        empty!(lcval);
+        #zf+=nR;
     end
     return cR;
 end
@@ -155,6 +246,7 @@ function recovery(degFT::degF{1,:H1}, degFR::degF{1,:H1}, cval::Array{Float64,1}
     return cR;
 end
 =#
+
 function recovery(degFT::degF{2,:H1div}, degFR::degF{2,:H1xH1}, cval::Array{Float64,1},
                   stencil, m::mesh, kubPoints::Array{Float64,2}, kubWeights::Array{Float64,2})
     phiT=@views degFT.phi;
