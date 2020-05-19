0b301274e02c62a16d71069fc10fdee71db0a94d
rug-levin <levinrug98@gmail.com>
2020-05-19 19:34:26 +0200

    removed allocations and unnecessary warning

--- correctNormalsCompound.jl
+++ correctNormalsCompound.jl
@@ -1,11 +1,11 @@
 function correctNormalsCompound!(n1::Array{Float64,1},n2::Array{Float64,1},eT1::Int64,eT2::Int64,compoundData::compoundData{:RectToKites})
-    if in(eT1,[3,4])
+    if eT1>=3
         n1[1]=-n1[1];
         n1[2]=-n1[2];
     else
         #n1=n1;
     end
-    if in(eT2,[3,4])
+    if eT2>=3
         n2[1]=-n2[1];
         n2[2]=-n2[2];
     else
@@ -15,14 +15,13 @@ end
 
 
 function correctNormalsCompound!(n1::Array{Float64,1},n2::Array{Float64,1},eT1::Int64,eT2::Int64,compoundData::compoundData)
-    @warn("wrong normalcorrection")
-    if in(eT1,[4,5,6])
+    if eT1>=4
         n1[1]=-n1[1];
         n1[2]=-n1[2];
     else
         #n1=n1;
     end
-    if in(eT2,[4,5,6])
+    if eT2>=4
         n2[1]=-n2[1];
         n2[2]=-n2[2];
     else
