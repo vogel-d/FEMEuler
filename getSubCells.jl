b683b1ad21962feee30ce88ad3fd19b87c2972c3
rug-levin <levinrug98@gmail.com>
2020-05-19 19:33:42 +0200

    removed allocations for hextotri

--- getSubCells.jl
+++ getSubCells.jl
@@ -1,12 +1,42 @@
-function getSubCells!(subcoord::Array{Array{Float64,2},1}, coord::Array{Float64,2}, compoundData::compoundData{:HexToTris})
-    middle=(1/6).*sum(coord,dims=2); #allocation
+function getSubCells!(subcoord::Array{Array{Float64,2},1}, coord::Array{Float64,2}, center::Array{Float64,1}, compoundData::compoundData{:HexToTris})
+    #middle=(1/6).*sum(coord,dims=2); #allocation
+    fill!(center,0.0);
+    for i in 1:6
+        for j in 1:2
+            center[j]+=(1/6)*coord[j,i];
+        end
+    end
 
     for i in 1:6
-        subcoord[(i-1)*2+1]= [coord[1,i] 0.5*(coord[1,i]+coord[1,1+mod(i,6)]) middle[1];
-                              coord[2,i] 0.5*(coord[2,i]+coord[2,1+mod(i,6)]) middle[2]];
+        #subcoord[(i-1)*2+1]= [coord[1,i] 0.5*(coord[1,i]+coord[1,1+mod(i,6)]) middle[1];
+        #                      coord[2,i] 0.5*(coord[2,i]+coord[2,1+mod(i,6)]) middle[2]];
 
-        subcoord[i*2]=       [0.5*(coord[1,i]+coord[1,1+mod(i,6)]) coord[1,1+mod(i,6)] middle[1];
-                              0.5*(coord[2,i]+coord[2,1+mod(i,6)]) coord[2,1+mod(i,6)] middle[2]];
+        #subcoord[i*2]=       [0.5*(coord[1,i]+coord[1,1+mod(i,6)]) coord[1,1+mod(i,6)] middle[1];
+        #                      0.5*(coord[2,i]+coord[2,1+mod(i,6)]) coord[2,1+mod(i,6)] middle[2]];
+
+        for j in 1:2
+          subcoord[(i-1)*2+1][j,1]=coord[j,i]
+        end
+
+        for j in 1:2
+          subcoord[(i-1)*2+1][j,2]=0.5*(coord[j,i]+coord[j,1+mod(i,6)])
+        end
+
+        for j in 1:2
+          subcoord[(i-1)*2+1][j,3]=center[j]
+        end
+
+        for j in 1:2
+          subcoord[i*2][j,1]=0.5*(coord[j,i]+coord[j,1+mod(i,6)])
+        end
+
+        for j in 1:2
+          subcoord[i*2][j,2]=coord[j,1+mod(i,6)]
+        end
+
+        for j in 1:2
+          subcoord[i*2][j,3]=center[j]
+        end
     end
 end
 
