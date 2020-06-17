a8f39c8e12cc59576d64b3969f14c7407e0c9956
vogel-d <denise.vogel@gmx.net>
2020-06-17 13:37:23 +0200

    deleted prints

--- symplektischerEulerCEAdv.jl
+++ symplektischerEulerCEAdv.jl
@@ -4,17 +4,7 @@ function symplektischerEuler!(y::solution,p::femProblem,w::solution,ns::Int64,dt
   numRho=p.degFBoundary[p.femType[:rho][1]].num
   numRhoV=p.degFBoundary[p.femType[:rhoV][1]].num
   numRhoTheta=p.degFBoundary[p.femType[:rhoTheta][1]].num
-  #=
-  println(" ")
-  println(sum(w.rhoV[1:numRhoV]))
-  println(sum(w.rho[1:numRho]))
-  println(sum(w.rhoTheta[1:numRhoTheta]))
-  println(" ")
-  vtk(p.mesh,p.degFBoundary[p.femType[:rhoV][1]],w.rhoV[1:numRhoV],p.femType[:rhoV][1],"testAdvRhoV")
-  vtk(p.mesh,p.degFBoundary[p.femType[:rho][1]],w.rho[1:numRho],p.femType[:rho][1],"testAdvRho")
-  vtk(p.mesh,p.degFBoundary[p.femType[:rhoTheta][1]],w.rhoTheta[1:numRhoTheta],p.femType[:rhoTheta][1],"testAdvRhoTheta")
-  println(vhvgv)
-  =#
+
   for i in 1:ns
     copy!(velOld,y.rhoV);
     @. y.rhoV[1:numRhoV]+=dtau*(w.rhoV[1:numRhoV]);
