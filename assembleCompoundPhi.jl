cb8dc0abaf138f8ec1b53008c372d7d249415a7c
rug-levin <levinrug98@gmail.com>
2020-06-29 11:27:02 +0200

    added RectToTri compound method

--- assembleCompoundPhi.jl
+++ assembleCompoundPhi.jl
@@ -1,13 +1,17 @@
-function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{Array{Float64,2},1}, degF::degF{1,:H1}, m::mesh, J, dJ, phi, kubPoints, kubWeights, compoundData::compoundData{:HexToTris})
+function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, compoundData::compoundData{:HexToTris})
     fill!(assembledPhi,ones(Float64,1,12));
 end
 
-function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{Array{Float64,2},1}, degF::degF{1,:H1}, m::mesh, J, dJ, phi, kubPoints, kubWeights, compoundData::compoundData{:HexToKites})
+function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, compoundData::compoundData{:HexToKites})
     fill!(assembledPhi,ones(Float64,1,6));
 end
 
-function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{Array{Float64,2},1}, degF::degF{1,:H1}, m::mesh, J, dJ, phi, kubPoints, kubWeights, compoundData::compoundData{:RectToKites})
+function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, compoundData::compoundData{:RectToKites})
     fill!(assembledPhi,ones(Float64,1,4));
+end
+
+function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, compoundData::compoundData{:RectToTris})
+    fill!(assembledPhi,ones(Float64,1,8));
 end
 
 
@@ -293,6 +297,99 @@ function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{A
 end
 
 
+function assemblePhi!(assembledPhi::Array{Array{Float64,2},1}, subcoord::Array{Array{Float64,2},1}, m::mesh, divphi, J, ddJ, jphi, nquadPhi, nquadPoints, quadWeights, compoundData::compoundData{:RectToTris})
+    mt=m.meshType;
+
+    nPhiSubCell=size(jphi,2);
+    nSubCells=compoundData.nSubCells;
+    A=Array{Float64,2}(undef,nPhiSubCell*nSubCells,nPhiSubCell*nSubCells);
+    fill!(A,0.0)
+    b=Array{Float64,1}(undef,nPhiSubCell*nSubCells);
+    fill!(b,0.0)
+    tangent=Array{Float64,1}(undef,2);
+    divCoeff=Array{Float64,1}(undef,nSubCells);
+
+    #first constraint
+    #outer edges
+    for i in 1:8
+        A[i,i]=1.0;
+    end
+    b[1]=1.0;
+    b[2]=1.0;
+
+    #inner edges
+    #last two subCell-ansatzfunctions correspond to inner edges
+    #so second to last beta from one subCell should match last beta from next subCell
+    for i in 1:8
+        #add second to last beta from current subCell
+        A[8+i,8+i]=1.0;
+        #add last from next subCell
+        A[8+i,16+mod(i,8)+1]=-1.0;
+    end
+
+    dotp=0.0;
+    for subCell in 1:8
+        for i in 1:2
+            tangent[i]=subcoord[subCell][i,2]-subcoord[subCell][i,1];
+        end
+        @. tangent=tangent/sqrt(tangent[1]^2+tangent[2]^2);
+        jacobi!(J,ddJ,jphi,nquadPoints[1],nquadPhi[1],subcoord[subCell],mt);
+        for i in 1:nPhiSubCell
+            for r in 1:length(quadWeights)
+                dotp=0.0;
+                for d in 1:m.geometry.dim
+                    dotp+=jphi[d,i][r]*tangent[d];
+                end
+                #apply third constraint
+                A[end, (i-1)*8+subCell]+=quadWeights[r]*(ddJ[r]/abs(ddJ[r]))*dotp;
+                #saving ddJ for divergence-constraint
+                divCoeff[subCell]=copy(ddJ[1]);
+                if divCoeff[subCell]<0.0
+                    println("negative determinant while assembling phi. not considered");
+                end
+            end
+        end
+    end
+    #println(A[24,:])
+
+    #TODO: check where to compute divergence since its non-constant
+    divCoeff=ones(nSubCells);
+    #apply second constraint (divergence)
+    for i in 1:7
+        #FEMEuler divergences
+        A[16+i,1]=      1*divCoeff[1];
+        A[16+i,8+1]=    1*divCoeff[1];
+        A[16+i,16+1]=    (-1)*divCoeff[1];
+
+        A[16+i,1+i]=    (-1)*divCoeff[i+1];
+        A[16+i,8+1+i]=  (-1)*divCoeff[i+1];
+        A[16+i,16+1+i]=  1*divCoeff[i+1];
+        #MelvinThuburn divergences (common divergences)
+    #    A[24+i,[1,12+1,24+1]]=[1.0,1.0,1.0];
+    #    A[24+i,[1+i,12+1+i,24+1+i]]=[-1.0,-1.0,-1.0];
+    end
+
+    betas= A\b;
+global betaz=betas
+global Az=A
+    for compoundPhi in 1:2
+        for subPhi in 1:nPhiSubCell
+            for subElement in 1:nSubCells
+                #                                                                   walking through 1:8 cyclic; starting from 1, then 11, then 9, ...
+                assembledPhi[compoundPhi][subPhi,subElement] = betas[(subPhi-1)*8+(1+mod(-1+subElement-2*(compoundPhi-1),8))]
+                #assembledPhi[compoundPhi][subPhi,subElement] = betas[(subPhi-1)*8+]
+            end
+        end
+    end
+    for compoundPhi in 3:4
+        for subPhi in 1:nPhiSubCell
+            for subElement in 1:nSubCells
+                #                                                                   walking through 1:8 cyclic; starting from 1, then 11, then 9, ...
+                assembledPhi[compoundPhi][subPhi,subElement] = -betas[(subPhi-1)*8+(1+mod(-1+subElement-2*(compoundPhi-1),8))]
+            end
+        end
+    end
+end
 
 
 
