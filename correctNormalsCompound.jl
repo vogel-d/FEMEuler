3dd8f71f33c9b908762db78db660ac54e96e98d1
rug-levin <levinrug98@gmail.com>
2020-06-29 11:30:39 +0200

    added RectToTri method and minor changes

--- correctNormalsCompound.jl
+++ correctNormalsCompound.jl
@@ -13,6 +13,21 @@ function correctNormalsCompound!(n1::Array{Float64,1},n2::Array{Float64,1},eT1::
     end
 end
 
+function correctNormalsCompound!(n1::Array{Float64,1},n2::Array{Float64,1},eT1::Int64,eT2::Int64,compoundData::compoundData{:RectToTris})
+    if eT1>=3
+        n1[1]=-n1[1];
+        n1[2]=-n1[2];
+    else
+        #n1=n1;
+    end
+    if eT2>=3
+        n2[1]=-n2[1];
+        n2[2]=-n2[2];
+    else
+        #n2=n2;
+    end
+end
+
 
 function correctNormalsCompound!(n1::Array{Float64,1},n2::Array{Float64,1},eT1::Int64,eT2::Int64,compoundData::compoundData)
     if eT1>=4
@@ -27,4 +42,18 @@ function correctNormalsCompound!(n1::Array{Float64,1},n2::Array{Float64,1},eT1::
     else
         #n2=n2;
     end
+#=
+    if in(eT1,[1,5,6])
+        n1[1]=-n1[1];
+        n1[2]=-n1[2];
+    else
+        #n1=n1;
+    end
+    if in(eT2,[1,5,6])
+        n2[1]=-n2[1];
+        n2[2]=-n2[2];
+    else
+        #n2=n2;
+    end
+=#
 end
