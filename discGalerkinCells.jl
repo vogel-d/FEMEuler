6d072e9b5041ac14ee9e974f102e090c9a88b96b
rug-levin <levinrug98@gmail.com>
2020-06-29 11:32:47 +0200

    added vectorial advection matrix-routine

--- discGalerkinCells.jl
+++ discGalerkinCells.jl
@@ -137,7 +137,7 @@ function discGalerkinCells!(M::Array{Float64,2},
 
     for k in 1:m.topology.size[3]
         jacobi!(ddJ,jphiT,m,k,kubPoints,phiT,coord);
-        
+
         l2g!(globalNumW,degFW,k);
 
         fill!(w1,0.0);
@@ -176,7 +176,82 @@ function discGalerkinCells!(M::Array{Float64,2},
             end
             M[gi]-=z;
         end
+    end
+    return nothing;
+end
 
+
+function discGalerkinCells!(rows::Array{Int64,1}, cols::Array{Int64,1}, vals::Array{Float64,1},
+                            degFT::degF{2,:H1div},phiT::Array{Array{Float64,2},2}, globalNumT::Array{Int64,1},
+                            degFF::degF{2,:H1div},phiF::Array{Array{Float64,2},2}, dphiF::Array{Array{Float64,2},1}, fval::SparseVector{Float64,Int64}, globalNumF::Array{Int64,1},
+                            degFW::degF{2,:H1div},phiW::Array{Array{Float64,2},2}, gradphiW::Array{Array{Float64,2},2}, wval::Array{Float64,1}, globalNumW::Array{Int64,1},
+                            #degFW::degF{2,S} where S,phiW::Array{Array{Float64,2},2}, gradphiW::Array{Array{Float64,2},2}, wval::Array{Float64,1}, globalNumW::Array{Int64,1},
+                            m::mesh, kubPoints::Array{Float64,2}, kubWeights::Array{Float64,2}, coord::Array{Float64,2})
+
+    sk=size(kubWeights);
+    nT=size(phiT,2);
+    nF=size(phiF,2);
+
+    lM=zeros(nT,nF);
+
+    ddJ=Array{Float64,2}(undef,sk);
+    jphiT=initJacobi(size(phiT),sk);
+
+    w1=zeros(sk);
+    w2=zeros(sk);
+    gradw11=zeros(sk);
+    gradw12=zeros(sk);
+    gradw21=zeros(sk);
+    gradw22=zeros(sk);
+
+    for k in 1:m.topology.size[3]
+        jacobi!(ddJ,jphiT,m,k,kubPoints,phiT,coord);
+
+        l2g!(globalNumW,degFW,k);
+
+        fill!(w1,0.0);
+        fill!(w2,0.0);
+        for i in 1:length(globalNumW)
+            @. w1+=wval[globalNumW[i]]*phiW[1,i];
+            @. w2+=wval[globalNumW[i]]*phiW[2,i];
+        end
+
+        fill!(gradw11,0.0);
+        fill!(gradw12,0.0);
+        fill!(gradw21,0.0);
+        fill!(gradw22,0.0);
+        zg=0;
+        for i in 1:size(phiW,2)
+            @. gradw11+=wval[globalNumW[i]]*gradphiW[1,1+zg];
+            @. gradw12+=wval[globalNumW[i]]*gradphiW[1,2+zg];
+            @. gradw21+=wval[globalNumW[i]]*gradphiW[2,1+zg];
+            @. gradw22+=wval[globalNumW[i]]*gradphiW[2,2+zg];
+            zg+=2;
+        end
+
+
+        l2g!(globalNumF,degFF,k);
+        l2g!(globalNumT,degFT,k);
+        fill!(lM,0.0);
+        for i in 1:length(globalNumT)
+            gi=globalNumT[i];
+            for j in 1:length(globalNumF)
+                gj=globalNumF[j];
+                for r in 1:size(kubWeights,2)
+                    for l in 1:size(kubWeights,1)
+                        lM[i,j]+=kubWeights[l,r]*(ddJ[l,r]^3/abs(ddJ[l,r]))*(jphiT[1,i][l,r]*(dphiF[j][l,r]*w1[l,r]+gradw11[l,r]*phiF[1,j][l,r]+gradw12[l,r]*phiF[2,j][l,r])+jphiT[2,i][l,r]*(dphiF[j][l,r]*w2[l,r]+gradw21[l,r]*phiF[1,j][l,r]+gradw22[l,r]*phiF[2,j][l,r]));
+                    end
+                end
+            end
+        end
+        for i in 1:length(globalNumT)
+            gi=globalNumT[i];
+            for j in 1:length(globalNumF)
+                push!(rows,gi);
+                push!(cols,globalNumF[j]);
+                push!(vals,-lM[i,j]);
+            end
+        end
     end
     return nothing;
 end
--- discGalerkinEdges.jl
+++ discGalerkinEdges.jl
@@ -313,6 +313,133 @@ function discGalerkinEdges!(M::Array{Float64,2},
 end
 
 
+function discGalerkinEdges!(rows::Array{Int64,1}, cols::Array{Int64,1}, vals::Array{Float64,1},
+                            degFT::degF{2,:H1div},phiT::Array{Array{Float64,2},2}, phiTtrans::Array{Array{Array{Float64,1},2},1}, globalNumT1::Array{Int64,1}, globalNumT2::Array{Int64,1},
+                            degFF::degF{2,:H1div},phiF::Array{Array{Float64,2},2}, phiFtrans::Array{Array{Array{Float64,1},2},1}, fval::SparseVector{Float64,Int64}, globalNumF1::Array{Int64,1}, globalNumF2::Array{Int64,1},
+                            degFW::degF{2,:H1div},phiW::Array{Array{Float64,2},2}, phiWtrans::Array{Array{Array{Float64,1},2},1}, wval::Array{Float64,1}, globalNumW1::Array{Int64,1}, globalNumW2::Array{Int64,1},
+                            #degFW::degF{2,S} where S,phiW::Array{Array{Float64,2},2}, phiWtrans::Array{Array{Array{Float64,1},2},1}, wval::Array{Float64,1}, globalNumW1::Array{Int64,1}, globalNumW2::Array{Int64,1},
+                            m::mesh, quadWeights::Array{Float64,1}, nquadPoints::Array{Array{Float64,2},1}, edgeData::Array{Array{Int64,1},1},gamma::Float64, coord::Array{Float64,2})
+
+    nT=size(phiT,2);
+    nF=size(phiF,2);
+    sk=length(quadWeights)
+    J1=initJacobi((m.geometry.dim,m.topology.dim),sk);
+    ddJ1=Array{Float64,1}(undef,sk);
+    jphiWn1=initJacobi((m.geometry.dim,size(phiW,2)),sk)
+    jphiTn1=initJacobi((m.geometry.dim,size(phiT,2)),sk)
+
+    J2=initJacobi((m.geometry.dim,m.topology.dim),sk);
+    ddJ2=Array{Float64,1}(undef,sk);
+    jphiWn2=initJacobi((m.geometry.dim,size(phiW,2)),sk)
+    jphiTn2=initJacobi((m.geometry.dim,size(phiT,2)),sk);
+
+    w1=[zeros(sk) for d in 1:m.geometry.dim]
+    w2=[zeros(sk) for d in 1:m.geometry.dim]
+
+    lM11=zeros(nT,nF);
+    lM12=zeros(nT,nF);
+    lM21=zeros(nT,nF);
+    lM22=zeros(nT,nF);
+
+    z=1;
+    for e in 1:length(edgeData[1])
+        inc1=edgeData[2][z];
+        inc2=edgeData[2][z+1];
+        eT1=edgeData[3][z];
+        eT2=edgeData[3][z+1];
+        z+=2;
+        n1=@views m.normals[:,eT1];
+        n2=@views m.normals[:,eT2];
+        le=m.edgeLength[edgeData[1][e]];
+        globv=@views edgeData[4][edgeData[5][e]:edgeData[5][e+1]-1];
+
+        phiFn1=@views phiFtrans[eT1];
+        phiTn1=@views phiTtrans[eT1];
+        phiWn1=@views phiWtrans[eT1];
+        kubPn1=@views nquadPoints[eT1];
+        phiFn2=@views phiFtrans[eT2];
+        phiTn2=@views phiTtrans[eT2];
+        phiWn2=@views phiWtrans[eT2];
+        kubPn2=@views nquadPoints[eT2];
+
+        jacobi!(J1,ddJ1,jphiWn1,jphiTn1,m,inc1,kubPn1, phiWn1, phiTn1, coord);
+        jacobi!(J2,ddJ2,jphiWn2,jphiTn2,m,inc2,kubPn2, phiWn2, phiTn2, coord);
+
+
+        l2g!(globalNumW1,degFW,inc1);
+        l2g!(globalNumW2,degFW,inc2);
+        for d in 1:m.geometry.dim
+            fill!(w1[d],0.0);
+            fill!(w2[d],0.0);
+            for i in 1:length(globalNumW1)
+                @. w1[d]+=wval[globalNumW1[i]]*jphiWn1[d,i];
+                @. w2[d]+=wval[globalNumW2[i]]*jphiWn2[d,i];
+            end
+        end
+
+        s=0.0;
+        for i in globv
+            s+=fval[i];
+        end
+        s<=0.0 ? gammaLoc=-gamma : gammaLoc=gamma;
+
+        fill!(lM11,0.0);
+        fill!(lM12,0.0);
+        fill!(lM21,0.0);
+        fill!(lM22,0.0);
+        for j in 1:nF
+            for i in 1:nT
+                for r in 1:length(quadWeights)
+                    w1jphiTn1=0.0; w2jphiTn1=0.0; w1jphiTn2=0.0; w2jphiTn2=0.0;
+                    for d in 1:m.geometry.dim
+                        w1jphiTn1+=w1[d][r]*jphiTn1[d,i][r];
+                        w2jphiTn1+=w2[d][r]*jphiTn1[d,i][r];
+                        w1jphiTn2+=w1[d][r]*jphiTn2[d,i][r];
+                        w2jphiTn2+=w2[d][r]*jphiTn2[d,i][r];
+                    end
+                    lM11[i,j]+=quadWeights[r]*ddJ1[r]*ddJ1[r]*(n1[1]*phiFn1[1,j][r]+n1[2]*phiFn1[2,j][r])*w1jphiTn1;
+                    lM12[i,j]+=quadWeights[r]*ddJ2[r]*ddJ1[r]*(n2[1]*phiFn2[1,j][r]+n2[2]*phiFn2[2,j][r])*w2jphiTn1;
+                    lM21[i,j]+=quadWeights[r]*ddJ1[r]*ddJ2[r]*(n1[1]*phiFn1[1,j][r]+n1[2]*phiFn1[2,j][r])*w1jphiTn2;
+                    lM22[i,j]+=quadWeights[r]*ddJ2[r]*ddJ2[r]*(n2[1]*phiFn2[1,j][r]+n2[2]*phiFn2[2,j][r])*w2jphiTn2;
+                    # piola: nphiF mit 1/Je = 1/Kantenlänge, phiW und phiT mit 1/dJ*J
+                end
+            end
+        end
+
+        l2g!(globalNumF1,degFF,inc1);
+        l2g!(globalNumT1,degFT,inc1);
+        l2g!(globalNumF2,degFF,inc2);
+        l2g!(globalNumT2,degFT,inc2);
+
+        for i in 1:length(globalNumT1)
+            gi1=globalNumT1[i];
+            gi2=globalNumT2[i];
+            for j in 1:length(globalNumF2)
+                gj1=globalNumF1[j];
+                gj2=globalNumF2[j];
+
+                push!(rows,gi1);
+                push!(cols,gj1);
+                push!(vals,(+0.5-gammaLoc)*lM11[i,j]);
+
+                push!(rows,gi1);
+                push!(cols,gj2);
+                push!(vals,(-0.5+gammaLoc)*lM12[i,j]);
+
+                push!(rows,gi2);
+                push!(cols,gj1);
+                push!(vals,(+0.5+gammaLoc)*lM21[i,j]);
+
+                push!(rows,gi2);
+                push!(cols,gj2);
+                push!(vals,(-0.5-gammaLoc)*lM22[i,j]);
+            end
+        end
+    end
+    return nothing;
+end
+
+
 function discGalerkinEdges!(M::Array{Float64,2},
                             degFT::degF{2,:H1div},phiT::Array{Array{Float64,2},2}, phiTtrans::Array{Array{Array{Float64,1},2},1}, globalNumT1::Array{Int64,1}, globalNumT2::Array{Int64,1},
                             degFF::degF{2,:H1div},phiF::Array{Array{Float64,2},2}, phiFtrans::Array{Array{Array{Float64,1},2},1}, fval::SparseVector{Float64,Int64}, globalNumF1::Array{Int64,1}, globalNumF2::Array{Int64,1},
