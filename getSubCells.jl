0e90b09b914ed7bca9c63c733853d02a60fc2d1f
rug-levin <levinrug98@gmail.com>
2020-06-29 11:27:32 +0200

    added RectToTri compound method

--- getSubCells.jl
+++ getSubCells.jl
@@ -98,3 +98,38 @@ function getSubCells!(subcoord::Array{Array{Float64,2},1}, coord::Array{Float64,
         #             0.5*(coord[2,mod(i+4,6)+1]+coord[2,i]) coord[2,i] 0.5*(coord[2,i]+coord[2,mod(i,6)+1]) center[2]]
     end
 end
+
+function getSubCells!(subcoord::Array{Array{Float64,2},1}, coord::Array{Float64,2}, center::Array{Float64,1}, compoundData::compoundData{:RectToTris})
+    #middle=(1/6).*sum(coord,dims=2);
+    fill!(center,0.0);
+    for i in 1:4
+        for j in 1:2
+            center[j]+=(1/4)*coord[j,i];
+        end
+    end
+    for i in 1:4
+        for j in 1:2
+            subcoord[(i-1)*2+1][j,1]=coord[j,i];
+        end
+
+        for j in 1:2
+            subcoord[(i-1)*2+1][j,2]=0.5*(coord[j,i]+coord[j,mod(i,4)+1])
+        end
+
+        for j in 1:2
+            subcoord[(i-1)*2+1][j,3]=center[j];
+        end
+
+        for j in 1:2
+            subcoord[2*i][j,1]=0.5*(coord[j,i]+coord[j,mod(i,4)+1])
+        end
+
+        for j in 1:2
+            subcoord[2*i][j,2]=coord[j,mod(i,4)+1];
+        end
+
+        for j in 1:2
+            subcoord[2*i][j,3]=center[j];
+        end
+    end
+end
