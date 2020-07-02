15e755f696629b532b327c823933101c9a47a644
vogel-d <denise.vogel@gmx.net>
2020-07-02 02:17:22 +0200

    added jacobi routine for stabdard case

--- jacobi.jl
+++ jacobi.jl
@@ -403,3 +403,57 @@ function jacobi!(J::Array{Array{Float64,1},2},ddJ::Array{Float64,1},jphi::Array{
 
     return nothing;
 end
+
+function jacobi!(J::Array{Array{Float64,1},2},ddJ::Array{Float64,1},jphi::Array{Array{Float64,1},2},jpsi::Array{Array{Float64,1},2},m::mesh, fid::Int, kubPoints::Array{Float64,2}, phi::Array{Array{Float64,1},2}, psi::Array{Array{Float64,1},2}, coord::Array{Float64,2})
+    key="20";
+    mt=m.meshType;
+    rstart=m.topology.offset[key][fid];
+    z=1;
+    for j in rstart:rstart+mt-1
+        for i in 1:2
+            coord[i,z]=m.geometry.coordinates[i,m.topology.incidence[key][j]];
+        end
+        z+=1;
+    end
+
+    sk=size(kubPoints,2);
+
+    if mt==3
+        a=coord[:,2]-coord[:,1];
+        b=coord[:,3]-coord[:,1];
+        for i in 1:sk
+            ddJ[i]=1/(a[1]*b[2]-b[1]*a[2]);
+            J[1,1][i]=a[1];
+            J[1,2][i]=b[1];
+            J[2,1][i]=a[2];
+            J[2,2][i]=b[2];
+
+            for k in 1:size(phi,2)
+                jphi[1,k][i]=(J[1,1][i]*phi[1,k][i]+J[1,2][i]*phi[2,k][i]);
+                jphi[2,k][i]=(J[2,1][i]*phi[1,k][i]+J[2,2][i]*phi[2,k][i]);
+            end
+            for k in 1:size(psi,2)
+                jpsi[1,k][i]=(J[1,1][i]*psi[1,k][i]+J[1,2][i]*psi[2,k][i]);
+                jpsi[2,k][i]=(J[2,1][i]*psi[1,k][i]+J[2,2][i]*psi[2,k][i]);
+            end
+        end
+    elseif mt==4
+        for i=1:sk
+            J[1,1][i]=(coord[1,2]-coord[1,1])+(coord[1,3]-coord[1,4]-coord[1,2]+coord[1,1])*kubPoints[2,i];
+            J[2,1][i]=(coord[2,2]-coord[2,1])+(coord[2,3]-coord[2,4]-coord[2,2]+coord[2,1])*kubPoints[2,i];
+            J[1,2][i]=(coord[1,4]-coord[1,1])+(coord[1,3]-coord[1,4]-coord[1,2]+coord[1,1])*kubPoints[1,i];
+            J[2,2][i]=(coord[2,4]-coord[2,1])+(coord[2,3]-coord[2,4]-coord[2,2]+coord[2,1])*kubPoints[1,i];
+            ddJ[i]=1/abs(J[1,1][i]*J[2,2][i]-J[2,1][i]*J[1,2][i]);
+            for k in 1:size(phi,2)
+                jphi[1,k][i]=(J[1,1][i]*phi[1,k][i]+J[1,2][i]*phi[2,k][i]);
+                jphi[2,k][i]=(J[2,1][i]*phi[1,k][i]+J[2,2][i]*phi[2,k][i]);
+            end
+            for k in 1:size(psi,2)
+                jpsi[1,k][i]=(J[1,1][i]*psi[1,k][i]+J[1,2][i]*psi[2,k][i]);
+                jpsi[2,k][i]=(J[2,1][i]*psi[1,k][i]+J[2,2][i]*psi[2,k][i]);
+            end
+        end
+    end
+
+    return nothing;
+end
