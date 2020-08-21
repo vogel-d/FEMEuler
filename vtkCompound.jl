64e476e2a04195e1e2f571fa36428a063e98545e
rug-levin <levinrug98@gmail.com>
2020-08-21 10:30:14 +0200

    added single timestep saving for scalars

--- vtkCompound.jl
+++ vtkCompound.jl
@@ -557,3 +557,64 @@ function compound_vtk(m::mesh, degF::degF{2}, compoundData::compoundData, sol::A
     outfiles=vtk_save(vtk);
     return outfiles::Vector{String}
 end
+
+
+function compound_vtk(m::mesh, degF::degF{1}, compoundData::compoundData, sol::Array{Float64,1}, femType::Symbol, filename::String, name::String="Test")
+
+    pts=m.geometry.coordinates;
+
+    Npts=size(pts,2);
+
+    if m.meshType==4
+        celltype = VTKCellTypes.VTK_QUAD;
+        mx=0.5;
+        my=0.5;
+    elseif m.meshType==3
+        celltype = VTKCellTypes.VTK_TRIANGLE;
+        mx=0.3333333333333333;
+        my=0.3333333333333333;
+    else
+        error("Ungültiger Mesh-Typ")
+    end
+    if m.geometry.dim==3
+        celltype = VTKCellTypes.VTK_POLYGON;
+    end
+    if typeof(compoundData).parameters[1]!=:nomethod
+        celltype = VTKCellTypes.VTK_POLYGON;
+    end
+    cells = MeshCell[]
+    inc=m.topology.incidence["20"];
+    off=m.topology.offset["20"];
+    nf=m.topology.size[3];
+    for k in 1:nf
+        inds=inc[off[k]:off[k+1]-1];
+        push!(cells, MeshCell(celltype, inds))
+    end
+    fComp=getElementProperties(femType,m.meshType,mx,my);
+    J=Array{Float64,2}(undef,m.geometry.dim,m.topology.dim);
+    dJ=0.0;
+    coord=Array{Float64,2}(undef,m.geometry.dim,m.meshType);
+
+    vtk_filename_noext = (@__DIR__)*"/VTK/"*filename;
+    vtk = vtk_grid(vtk_filename_noext, pts, cells,compress=3)
+
+    #=
+    for k in 1:nf
+        cLoc=sol[l2g(degF, k)]
+        dJ=jacobi!(J,m,k,mx,my,coord);
+        fLoc=(1/dJ)*J*fComp
+        cvtk[:,k]=fLoc*cLoc;
+    end
+    =#
+    nSubCells=compoundData.nSubCells;
+    cvtk=zeros(Float64, nf)
+    for k in 1:nf
+        cLoc=sol[l2g(degF, k)]
+        #cvtk[k]=dot(fComp[l],cLoc);
+        cvtk[k]=mean(cLoc);
+    end
+    vtk_cell_data(vtk, cvtk, name)
+
+    outfiles=vtk_save(vtk);
+    return outfiles::Vector{String}
+end
