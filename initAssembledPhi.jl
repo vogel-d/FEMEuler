2683d2dd70f70a6251fac9c34aeeff02176ea3b1
rug-levin <levinrug98@gmail.com>
2021-01-01 10:45:50 +0100

    (merge) added third argument (dim) to getElementProperties-call

--- initAssembledPhi.jl
+++ initAssembledPhi.jl
@@ -1,7 +1,8 @@
 function initAssembledPhi(nSubCells::Int64, femElements::Set{Symbol}, meshType::Int64, nCompoundPhi::Dict{Symbol,Int64})
     assembledPhi=Dict();
+    dim=2; #compound implementation for 2D only (at the moment)
     for femElement in femElements
-        phi,size=getElementProperties(femElement,meshType);
+        phi,size=getElementProperties(femElement,meshType,dim);
         assembledPhi[femElement]=Array{Array{Float64,2},1}(undef,nCompoundPhi[femElement]);
         for i in 1:nCompoundPhi[femElement]
             assembledPhi[femElement][i]=Array{Float64,2}(undef,size[2],nSubCells)
@@ -14,9 +15,10 @@ function initAssembledPhiPre(mesh::mesh, nSubCells::Int64, femElements::Set{Symb
     meshType=mesh.meshType;
     nCells=mesh.topology.size[mesh.topology.dim+1];
     assembledPhiPre=Dict();
+    dim=2; #compound implementation for 2D only (at the moment)
 
     for femElement in femElements
-        phi,size=getElementProperties(femElement,meshType);
+        phi,size=getElementProperties(femElement,meshType,dim);
         assembledPhiPre[femElement]=Array{Array{Array{Float64,2},1},1}(undef,nCells);
         for Cell in 1:nCells
             assembledPhiPre[femElement][Cell]=Array{Array{Float64,2},1}(undef,nCompoundPhi[femElement])
